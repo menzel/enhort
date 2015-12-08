72511930eb53d2985a0ddfda6d930cf4dfb334ef
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-08 12:57:39 +0100

    loading bg and user dat

--- src/de/thm/run/Main.java
+++ src/de/thm/run/Main.java
@@ -1,8 +1,19 @@
 package de.thm.run;
 
+import de.thm.positionData.SimpleBackgroundModel;
+import de.thm.positionData.Sites;
+import de.thm.positionData.UserData;
+
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        UserData userDat = new UserData();
+        userDat.loadPositionsFromFile(new File("/home/menzel/Desktop/THM/lfba/projekphase/genomic_sites.sleeping_beauty.hg19.txt"));
+
+        Sites bg = new SimpleBackgroundModel(userDat.getPositionCount());
+
     }
 }
