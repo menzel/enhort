f98b92f1851038b249328060b2d55aba82246c3d
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-08 16:31:51 +0100

    added break for speed up

--- src/de/thm/calc/Intersect.java
+++ src/de/thm/calc/Intersect.java
@@ -3,42 +3,40 @@ package de.thm.calc;
 import de.thm.genomeData.Interval;
 import de.thm.positionData.Sites;
 
-import java.util.Iterator;
-
 /**
  * Created by Michael Menzel on 8/12/15.
  */
 public class Intersect {
 
-    public int searchSingleIntervall(Interval intv, Sites pos){
+    public Result searchSingleIntervall(Interval intv, Sites pos){
         int in = 0;
+        int out = 0;
+
 
         for(String chromosom: pos.getPositions().keySet()){
             long c = 0;
-            System.out.println("in " + chromosom);
 
             for(Long p: pos.getPositions().get(chromosom)){
 
-               Iterator<Long> it = intv.getIntervals().get(chromosom).iterator();
-               c = it.next();
+               for(int i = 0; i < intv.getIntervals().get(chromosom).size(); i++){
 
-               for(int i = 0; it.hasNext(); i++){
+                   c = intv.getIntervals().get(chromosom).get(i);
 
                    if(p < c){
                        //get one back to get interval start/stop
 
                        if((i-1)%2 == 0){ //last position was start
                            in++;
+                       }else{
+                           out++;
                        }
 
-                   }else{
-                       it.next();
+                       break;
                    }
-
                }
             }
         }
 
-        return in;
+        return new Result(in,out);
     }
 }
