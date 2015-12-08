6a0655f608f72d9341a4489c19e8fe42e1ede224
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-08 13:08:47 +0100

    added searchSingleInterval method

--- src/de/thm/calc/Intersect.java
+++ src/de/thm/calc/Intersect.java
@@ -1,7 +1,20 @@
 package de.thm.calc;
 
+import de.thm.genomeData.Interval;
+import de.thm.positionData.Sites;
+
 /**
  * Created by Michael Menzel on 8/12/15.
  */
 public class Intersect {
+
+    int searchSingleIntervall(Interval intv, Sites pos){
+        int in = 0;
+
+        if(intv.isIn(pos)){
+            in++;
+        }
+
+        return in;
+    }
 }
