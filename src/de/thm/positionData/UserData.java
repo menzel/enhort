bdcc53ef20cc6802418f908d0368a62dba3df19f
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-08 11:45:30 +0100

    Moved load from file method to userdata class

--- src/de/thm/positionData/UserData.java
+++ src/de/thm/positionData/UserData.java
@@ -1,7 +1,49 @@
 package de.thm.positionData;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
 /**
  * Created by Michael Menzel on 8/12/15.
  */
-public class UserData implements Sites{
+public class UserData extends Sites{
+        /**
+     *
+     * @param file
+     */
+    public void loadPositionsFromFile(File file){
+
+        initMap();
+
+        try(Stream<String> lines = Files.lines(file.toPath())){
+
+            Iterator it = lines.iterator();
+
+            while(it.hasNext()){
+                String line = (String) it.next();
+
+                positions.get(getChr(line)).add(getPosition(line));
+
+            }
+
+            lines.close();
+
+        } catch (IOException e ){
+            System.out.println(e);
+
+        }
+    }
+
+
+    private Integer getPosition(String line) {
+
+        return Integer.parseInt(line.split(" ")[1]);
+    }
+
+    private String getChr(String line) {
+        return line.split(" ")[0];
+    }
 }
