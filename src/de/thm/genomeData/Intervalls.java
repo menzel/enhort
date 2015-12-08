52a13e0091e32d309d5d1af9bbb3d493b473a83e
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-08 11:08:31 +0100

    genomeData and positionData classes

---
+++ src/de/thm/genomeData/Intervalls.java
@@ -0,0 +1,7 @@
+package de.thm.genomeData;
+
+/**
+ * Created by Michael Menzel on 8/12/15.
+ */
+public interface Intervalls {
+}
---
+++ src/de/thm/positionData/UserData.java
@@ -0,0 +1,7 @@
+package de.thm.positionData;
+
+/**
+ * Created by Michael Menzel on 8/12/15.
+ */
+public class UserData implements Sites{
+}
