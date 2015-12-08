d2e747a2770226735302edad0a760db313914f28
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-08 11:35:34 +0100

    methods for reading files and buidling map

--- src/de/thm/genomeData/Intervals.java
+++ src/de/thm/genomeData/Intervals.java
@@ -3,5 +3,5 @@ package de.thm.genomeData;
 /**
  * Created by Michael Menzel on 8/12/15.
  */
-public interface Intervalls {
+public interface Intervals {
 }
--- src/de/thm/positionData/Sites.java
+++ src/de/thm/positionData/Sites.java
@@ -1,7 +1,73 @@
 package de.thm.positionData;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Stream;
+
 /**
  * Created by Michael Menzel on 8/12/15.
  */
-public interface Sites {
+public abstract class Sites {
+    /**
+     *
+     */
+
+    private Map<String, ArrayList<Integer>> positions = new HashMap<>();
+
+    /**
+     *
+     * @param file
+     */
+    public void loadPositionsFromFile(File file){
+
+        initMap();
+
+        try(Stream<String> lines = Files.lines(file.toPath())){
+
+            Iterator it = lines.iterator();
+
+            while(it.hasNext()){
+                String line = (String) it.next();
+
+                positions.get(getChr(line)).add(getPosition(line));
+
+            }
+
+        } catch (IOException e ){
+            System.out.println(e);
+        }
+    }
+
+    /**
+     *
+     */
+    private void initMap(){
+
+        for(int i = 1; i <= 22; i++){
+            positions.put("chr"+i, new ArrayList<>());
+        }
+
+        positions.put("chrX", new ArrayList<>());
+        positions.put("chrY", new ArrayList<>());
+
+    }
+
+    protected abstract Integer getPosition(String line);
+
+    protected abstract String getChr(String line);
+
+
+
+    /*
+    Getter and Setter
+     */
+
+    public Map<String, ArrayList<Integer>> getPositions() {
+        return positions;
+    }
 }
