0c1aed3af50fa6ea6938254bdf3971ec41f721a6
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-09 10:42:47 +0100

    better rand generator

--- src/de/thm/positionData/SimpleBackgroundModel.java
+++ src/de/thm/positionData/SimpleBackgroundModel.java
@@ -3,6 +3,7 @@ package de.thm.positionData;
 import de.thm.misc.ChromosomSizes;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Random;
 
@@ -11,30 +12,37 @@ import java.util.Random;
  */
 public class SimpleBackgroundModel extends BackgroundModel{
 
+    private Random rand;
+
     public SimpleBackgroundModel(int sites) {
 
         initMap();
+        rand = new Random(System.currentTimeMillis());
         createSites(sites);
 
     }
 
     private void createSites(int sites) {
 
-        int perChr = sites/24;
+        int perChr = sites/positions.keySet().size();
 
         for (Map.Entry<String, ArrayList<Long>> entry: positions.entrySet()) {
 
             for(int i = 0 ; i < perChr; i++) {
-                entry.getValue().add(randomPosition(entry.getKey()));
+                long rand = randomPosition(entry.getKey());
+
+                entry.getValue().add(rand);
+                positionCount++;
             }
+
+            Collections.sort(entry.getValue());
 
         }
     }
 
     private Long randomPosition(String chr) {
-        Random rand =  new Random();
 
-        return rand.nextLong() * ChromosomSizes.getChrSize(chr);
+        return (long)(rand.nextDouble() * ChromosomSizes.getChrSize(chr));
     }
 
 }
