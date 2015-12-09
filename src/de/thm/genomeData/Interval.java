30cdac6a367a044331eb00c58c4b03db9e7b17d3
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-09 15:48:26 +0100

    template mehtod for load data

--- src/de/thm/genomeData/Interval.java
+++ src/de/thm/genomeData/Interval.java
@@ -1,8 +1,13 @@
 package de.thm.genomeData;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * Created by Michael Menzel on 8/12/15.
@@ -26,6 +31,33 @@ public abstract class Interval {
         intervals.put("chrY", new ArrayList<>());
 
     }
+    /**
+     *
+     * @param file
+     */
+    protected void loadIntervalData(File file){
+
+        try(Stream<String> lines = Files.lines(file.toPath())){
+            Iterator<String> it = lines.iterator();
+
+            while(it.hasNext()){
+                String line = it.next();
+                String[] parts = line.split("\t");
+
+
+                if(parts[1].matches("chr(\\d{1,2}|X|Y)")){ //TODO get other chromosoms
+
+                    handleParts(parts);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    protected abstract void handleParts(String[] parts);
 
 
     public Map<String, ArrayList<Long>> getIntervals() {
--- src/de/thm/genomeData/IntervalData.java
+++ src/de/thm/genomeData/IntervalData.java
@@ -1,10 +1,6 @@
 package de.thm.genomeData;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Iterator;
-import java.util.stream.Stream;
 
 /**
  * Created by Michael Menzel on 8/12/15.
@@ -17,36 +13,17 @@ public class IntervalData extends Interval{
      * @param file
      */
     public IntervalData(File file) {
+        super();
+
         initMap();
-        loadIntervalData(file);
     }
 
-    /**
-     *
-     * @param file
-     */
-    private void loadIntervalData(File file){
 
-        try(Stream<String> lines = Files.lines(file.toPath())){
-            Iterator<String> it = lines.iterator();
+    @Override
+    protected void handleParts(String[] parts) {
 
-            while(it.hasNext()){
-                String line = it.next();
-                String[] parts = line.split("\t");
-
-
-                if(parts[1].matches("chr(\\d{1,2}|X|Y)")){ //TODO get other chromosoms
-
-                    intervals.get(parts[1]).add(Long.parseLong(parts[3]));
-                    intervals.get(parts[1]).add(Long.parseLong(parts[4]));
-
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        intervals.get(parts[1]).add(Long.parseLong(parts[3]));
+        intervals.get(parts[1]).add(Long.parseLong(parts[4]));
 
     }
-
 }
