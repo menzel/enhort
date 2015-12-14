cdb4c8ab1701c4b38d6b629947942ce4efe79383
Michael Menzel <michael.menzel@mni.thm.de>
2015-12-14 11:26:00 +0100

    special getters for in, out and named list

--- src/de/thm/calc/Result.java
+++ src/de/thm/calc/Result.java
@@ -1,6 +1,7 @@
 package de.thm.calc;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Michael Menzel on 8/12/15.
@@ -10,15 +11,86 @@ public class Result {
 
     private final Map<String, Integer> resultNames;
     private final Map<Long, Integer> resultScores;
-    private final int classes = 1;
+    public enum Type {inout, score, named}
+    private Type type;
 
+    /**
+     *
+     * @return
+     */
+    public double[] getInOut() {
+        if(type == Type.inout)
+            return new double[] {getInCount(),resultNames.get("out")};
+
+        if(type == Type.named){
+
+            return getValues();
+        }
+
+        else //can only be Type.score, then getInOut should not be called.
+            return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private double[] getValues() {
+
+        List<String> names = resultNames.entrySet().stream().sorted().map(Map.Entry::getKey).collect(Collectors.toList());
+        double[] returnValues = new double[names.size()];
+
+        int i = 0;
+        for(String name: names){
+            returnValues[i++] = resultNames.get(name);
+        }
+
+        return returnValues;
+    }
+
+    /**
+     *
+     * @return
+     */
+    private double getInCount() {
+        double count = 0;
+
+        for(String name: resultNames.keySet()){
+            if(!name.matches("out"))
+                count += resultNames.get(name);
+        }
+
+        return count;
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public Type getType() {
+        if(type == Type.named && resultNames.keySet().size() == 2){
+            type = Type.inout;
+        }
+
+        return type;
+    }
+
+
+    /**
+     *
+     */
     public Result() {
         this.resultNames = new HashMap<>();
         this.resultScores= new HashMap<>();
     }
 
+    /**
+     *
+     * @param score
+     */
     public void add(long score){
-        score = (int) Math.round(score / classes);
+        if(type == null) type = Type.score;
 
         if(resultScores.containsKey(score)){
             resultScores.put(score, resultScores.get(score)+1);
@@ -27,7 +99,14 @@ public class Result {
         }
     }
 
+    /**
+     *
+     * @param name
+     * @param count
+     */
     public void add(String name, int count){
+
+        if(type == null) type = Type.named;
 
         if(resultNames.containsKey(name)){
             resultNames.put(name, resultNames.get(name)+count);
@@ -37,7 +116,13 @@ public class Result {
 
     }
 
+    /**
+     *
+     * @param name
+     */
     public void add(String name){
+
+        if(type == null) type = Type.named;
 
         if(resultNames.containsKey(name)){
             resultNames.put(name, resultNames.get(name)+1);
@@ -47,6 +132,10 @@ public class Result {
 
     }
 
+    /**
+     *
+     * @return
+     */
     public String getResultScores() {
         String val = "";
         List<Long> keys = new ArrayList<>(resultScores.keySet());
@@ -65,17 +154,6 @@ public class Result {
     @Override
     public String toString() {
         return resultNames.toString();
-    }
-
-    public int getA(){
-
-        return resultNames.get("in");
-
-    }
-
-    public int getB(){
-
-        return resultNames.get("out");
     }
 }
 
