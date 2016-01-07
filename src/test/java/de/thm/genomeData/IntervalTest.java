0e6dc4d2112413cd836c2234e195c2f0c554a718
Michael Menzel <michael.menzel@mni.thm.de>
2016-01-07 11:19:40 +0100

    Test offset handling for input

---
+++ src/test/java/de/thm/genomeData/IntervalTest.java
@@ -0,0 +1,35 @@
+package de.thm.genomeData;
+
+import de.thm.misc.ChromosomSizes;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Michael Menzel on 7/1/16.
+ */
+public class IntervalTest {
+
+    @Test
+    public void testHandleParts() throws Exception {
+        Interval intv = new IntervalNamed();
+        ChromosomSizes chrSizes = ChromosomSizes.getInstance();
+
+        intv.handleParts(mockParts("chr1", "10", "20", "name"));
+
+        assertEquals(10 + chrSizes.offset("chr1"), intv.getIntervalsStart().get(0).longValue());
+        assertEquals(20 + chrSizes.offset("chr1"), intv.getIntervalsEnd().get(0).longValue());
+
+    }
+
+    private String[] mockParts(String chr1, String s, String s1, String name) {
+        String[] parts = new String[4];
+
+        parts[0] = chr1;
+        parts[1] = s;
+        parts[2] = s1;
+        parts[3] = name;
+
+        return parts;
+    }
+}
