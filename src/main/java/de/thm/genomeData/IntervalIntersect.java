4db0ba21443ef7b1f626d690ab7a603be7815abf
Michael Menzel <michael.menzel@mni.thm.de>
2016-01-14 13:05:13 +0100

    Impl. interval intersect algorithm and test case

---
+++ src/main/java/de/thm/genomeData/IntervalIntersect.java
@@ -0,0 +1,60 @@
+package de.thm.genomeData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Michael Menzel on 13/1/16.
+ */
+public class IntervalIntersect {
+
+    public static Interval intersect(Interval intv1, Interval intv2){
+
+
+        List<Long> starts1 = intv1.getIntervalsStart();
+        List<Long> starts2 = intv2.getIntervalsStart();
+
+        List<Long> ends1 = intv1.getIntervalsEnd();
+        List<Long> ends2 = intv2.getIntervalsEnd();
+
+        Interval result = new Interval();
+        List<Long> result_start = new ArrayList<>();
+        List<Long> result_end = new ArrayList<>();
+
+        if(starts1.size() > starts2.size()){ //if start2 is smaller swap lists
+
+            starts2 = intv1.getIntervalsStart();
+            starts1 = intv2.getIntervalsStart();
+
+            ends2 = intv1.getIntervalsEnd();
+            ends1 = intv2.getIntervalsEnd();
+        }
+
+        // iterator through one of the intervals, check if a interval in the other track is overlapping with the current. if not proceed if yes create new interval
+        int i2 = 0;
+
+        for(int i1 = 0; i1 < starts1.size(); i1++){
+            for(; i2 < ends2.size(); i2++){
+
+                if(starts1.get(i1) < ends2.get(i2)){
+                    if(starts2.get(i2) < ends1.get(i1)){
+                        long start = Math.max(starts1.get(i1), starts2.get(i2));
+                        long end = Math.min(ends1.get(i1), ends2.get(i2));
+
+                        result_start.add(start);
+                        result_end.add(end);
+
+                    }
+                }
+
+                if(ends1.get(i1) < starts2.get(i2))
+                    break;
+            }
+        }
+
+        result.setIntervalsStart(result_start);
+        result.setIntervalsEnd(result_end);
+
+        return  result;
+    }
+}
---
+++ src/test/java/de/thm/genomeData/IntervalIntersectTest.java
@@ -0,0 +1,91 @@
+package de.thm.genomeData;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by Michael Menzel on 14/1/16.
+ */
+public class IntervalIntersectTest {
+
+    @Test
+    public void testIntersect() throws Exception {
+
+
+        List<Long> start1 = new ArrayList<>();
+        List<Long> start2 = new ArrayList<>();
+
+        List<Long> end1 = new ArrayList<>();
+        List<Long> end2 = new ArrayList<>();
+
+        start1.add(0L);
+        start1.add(13L);
+        start1.add(20L);
+        start1.add(42L);
+        start1.add(65L);
+
+        end1.add(12L);
+        end1.add(16L);
+        end1.add(25L);
+        end1.add(44L);
+        end1.add(70L);
+
+        start2.add(3L);
+        start2.add(7L);
+        start2.add(14L);
+        start2.add(28L);
+        start2.add(40L);
+        start2.add(70L);
+
+        end2.add(5L);
+        end2.add(9L);
+        end2.add(18L);
+        end2.add(32L);
+        end2.add(50L);
+        end2.add(80L);
+
+
+        Interval interval1 = mockInterval(start1, end1);
+        Interval interval2 = mockInterval(start2, end2);
+
+        Interval newInterval = IntervalIntersect.intersect(interval1,interval2);
+
+        /**
+        for(int i = 0; i < newInterval.getIntervalsStart().size(); i++){
+            System.out.print(newInterval.getIntervalsStart().get(i) + " - ");
+            System.out.println(newInterval.getIntervalsEnd().get(i));
+        }
+         **/
+
+        List<Long> expected_start = new ArrayList<>();
+        expected_start.add(3L);
+        expected_start.add(7L);
+        expected_start.add(14L);
+        expected_start.add(42L);
+
+        List<Long> expected_end = new ArrayList<>();
+        expected_end.add(5L);
+        expected_end.add(9L);
+        expected_end.add(16L);
+        expected_end.add(44L);
+
+        assertEquals(expected_start, newInterval.getIntervalsStart());
+        assertEquals(expected_end, newInterval.getIntervalsEnd());
+
+    }
+
+
+
+    private Interval mockInterval(List<Long> start, List<Long> end) {
+        Interval interval = new Interval();
+
+        interval.setIntervalsStart(start);
+        interval.setIntervalsEnd(end);
+
+        return interval;
+    }
+}
