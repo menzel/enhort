2768cc4cd43a3c25ad8e5c6ef0590ec06a07a8ff
Michael Menzel <michael.menzel@mni.thm.de>
2016-01-22 14:33:25 +0100

    fixed test

--- src/test/java/de/thm/genomeData/IntervalDumperTest.java
+++ src/test/java/de/thm/genomeData/IntervalDumperTest.java
@@ -19,30 +19,23 @@ public class IntervalDumperTest {
     public void setUp() throws Exception {
 
         basePath = "/home/menzel/Desktop/THM/lfba/projekphase/dat/";
-
-        long startTime = System.nanoTime();
         interval = new Interval(new File(basePath + "inout/exons.bed"), Interval.Type.inout);
-
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println("duration file " + duration/1000000);
-
-
         dumper = new IntervalDumper(new File("/tmp/").toPath());
+
+        File folder = new File("/tmp/kryo/");
+
+        if(folder.exists()){
+            folder.delete();
+        }
+
+        folder.mkdir();
     }
 
     @Test
     public void testGetInterval() throws Exception {
-        dumper.dumpInterval(interval, "foo");
 
-        long startTime = System.nanoTime();
-
-        Interval newInterval = dumper.getInterval(new File("/tmp/" + "foo"));
-
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        System.out.println("duration object " + duration/1000000);
-
+        dumper.dumpInterval(interval, "foo.bed");
+        Interval newInterval = dumper.getInterval(new File("/tmp/kryo/" + "foo.kryo"));
         assertEquals(interval.getIntervalsStart(), newInterval.getIntervalsStart());
     }
 }
