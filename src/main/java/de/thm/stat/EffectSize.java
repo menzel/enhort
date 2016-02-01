173e7a729693c51cdb25818a0d0c1396b0ad25a8
Michael Menzel <michael.menzel@mni.thm.de>
2016-02-01 17:29:04 +0100

    changed effectSize to fold change

--- src/main/java/de/thm/stat/EffectSize.java
+++ src/main/java/de/thm/stat/EffectSize.java
@@ -1,22 +1,19 @@
 package de.thm.stat;
 
 import de.thm.calc.IntersectResult;
-import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 
 /**
  * Created by Michael Menzel on 29/1/16.
  */
 public class EffectSize {
 
-    private PearsonsCorrelation correlation = new PearsonsCorrelation();
 
     public double test(IntersectResult intersectResultA, IntersectResult intersectResultB, String trackName) {
 
-        double[][] values= new double[2][2];
-        values[0] = new double[] {intersectResultA.getIn(), intersectResultA.getOut()};
-        values[1] = new double[] {intersectResultB.getIn(), intersectResultB.getOut()};
+        double fc1 = intersectResultA.getIn()/new Double(intersectResultA.getOut());
+        double fc2 = intersectResultB.getIn()/new Double(intersectResultB.getOut());
 
-        return correlation.correlation(values[0], values[1]);
+        return Math.abs(Math.log(fc1/fc2));
 
     }
 }
