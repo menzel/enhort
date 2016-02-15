7a9a1e4e7feff57ed1ede9e3b3406e4379574739
Michael Menzel <michael.menzel@mni.thm.de>
2016-02-15 15:50:30 +0100

    Statistics: Count each error

--- src/main/java/de/thm/exception/IntervalTypeNotAllowedExcpetion.java
+++ src/main/java/de/thm/exception/IntervalTypeNotAllowedExcpetion.java
@@ -1,10 +1,14 @@
 package de.thm.exception;
 
+import de.thm.spring.backend.StatisticsCollector;
+
 /**
  * Created by Michael Menzel on 15/2/16.
  */
 public class IntervalTypeNotAllowedExcpetion extends Throwable {
+
     public IntervalTypeNotAllowedExcpetion(String s) {
         super(s);
+        StatisticsCollector.getInstance().addErrorC();
     }
 }
--- src/main/java/de/thm/exception/TooManyCovariantsException.java
+++ src/main/java/de/thm/exception/TooManyCovariantsException.java
@@ -1,13 +1,18 @@
 package de.thm.exception;
 
+import de.thm.spring.backend.StatisticsCollector;
+
 /**
  * Created by Michael Menzel on 15/2/16.
  */
 public class TooManyCovariantsException extends Exception{
 
-    public TooManyCovariantsException() { }
+    public TooManyCovariantsException() {
+        StatisticsCollector.getInstance().addErrorC();
+    }
 
     public TooManyCovariantsException(String message) {
         super(message);
+        StatisticsCollector.getInstance().addErrorC();
     }
 }
