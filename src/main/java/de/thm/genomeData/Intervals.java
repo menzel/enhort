8a8bed89836ce4e1ff816f03e1b03fb561ad4975
Michael Menzel <michael.menzel@mni.thm.de>
2016-02-18 16:10:02 +0100

    created subset score method
    created combine method

--- src/main/java/de/thm/genomeData/Intervals.java
+++ src/main/java/de/thm/genomeData/Intervals.java
@@ -334,4 +334,136 @@ public class Intervals {
         return size;
     }
 
+    public static Interval subsetScore(Interval interval, double score) {
+        Interval n = new Interval();
+
+        List<Long> intervalStart = new ArrayList<>();
+        List<Long> intervalEnd = new ArrayList<>();
+
+        List<Double> intervalScore = interval.getIntervalScore();
+        List<Double> intervalScore_n = new ArrayList<>();
+
+        for (int i = 0; i < intervalScore.size(); i++) {
+
+            if (intervalScore.get(i) == score) {
+                intervalStart.add(interval.getIntervalsStart().get(i));
+                intervalEnd.add(interval.getIntervalsEnd().get(i));
+                intervalScore_n.add(score);
+            }
+        }
+
+        n.setIntervalScore(intervalScore_n);
+        n.setIntervalsStart(intervalStart);
+        n.setIntervalsEnd(intervalEnd);
+
+        return n;
+
+    }
+
+    public static Interval combine(List<Interval> intervals) {
+        if(intervals.size() == 0){
+            return null;
+
+        } else if(intervals.size() == 1){
+            return intervals.get(0);
+
+        } else if(intervals.size() == 2){
+            return combine(intervals.get(0), intervals.get(1));
+
+        }else{
+            List<Interval> newList = new ArrayList<>();
+            newList.addAll(intervals.subList(2, intervals.size()));
+
+            newList.add(combine(intervals.get(0), intervals.get(1)));
+
+            return combine(newList);
+        }
+
+    }
+
+    public static Interval combine(Interval intv1, Interval intv2) {
+
+        List<Long> starts1 = intv1.getIntervalsStart();
+        List<Long> starts2 = intv2.getIntervalsStart();
+
+        List<Long> ends1 = intv1.getIntervalsEnd();
+        List<Long> ends2 = intv2.getIntervalsEnd();
+
+        List<Double> scores1 = intv1.getIntervalScore();
+        List<Double> scores2 = intv2.getIntervalScore();
+
+        Interval result = new Interval();
+        result.setType(intv1.getType());
+        List<Long> result_start = new ArrayList<>();
+        List<Long> result_end = new ArrayList<>();
+        List<Double> result_score = new ArrayList<>();
+
+        if(starts1.size() > starts2.size()){ //if start2 is smaller swap lists
+
+            starts2 = intv1.getIntervalsStart();
+            starts1 = intv2.getIntervalsStart();
+
+            ends2 = intv1.getIntervalsEnd();
+            ends1 = intv2.getIntervalsEnd();
+
+            scores1 = intv2.getIntervalScore();
+            scores2 = intv1.getIntervalScore();
+        }
+
+
+        int i2 = 0;
+
+        for(int i1 = 0; i1 < starts1.size(); i1++) {
+            for (; i2 < ends2.size(); i2++) {
+
+                if(ends1.get(i1) < starts2.get(i2)) { //do not overlap
+                    result_start.add(starts1.get(i1));
+                    result_end.add(ends1.get(i1));
+                    result_score.add(scores1.get(i1));
+                    i1++;
+
+                    if(ends2.get(i2) < starts1.get(i1)) { //interval on second before next from s1
+                        result_start.add(starts2.get(i2));
+                        result_end.add(ends2.get(i2));
+                        result_score.add(scores2.get(i2));
+                        i2++;
+                    }
+
+                } else if(ends2.get(i2) > ends1.get(i1)){ //overlap
+                    result_start.add(starts1.get(i1));
+                    result_end.add(starts2.get(i2));
+                    result_score.add(scores1.get(i1));
+
+                    // do not assign overlapping part
+
+                    result_start.add(ends1.get(i1));
+                    result_end.add(ends2.get(i2));
+                    result_score.add(scores2.get(i2));
+                    i1++;
+
+                }  else { //second interval is inside the first
+                    //todo check if start and end are the same
+
+                    result_start.add(starts1.get(i1));
+                    result_end.add(starts2.get(i2));
+                    result_score.add(scores1.get(i1));
+
+                    result_start.add(ends2.get(i2));
+                    result_end.add(ends1.get(i1));
+                    result_score.add(scores1.get(i1));
+                }
+
+
+
+                if(i1 < starts1.size()-1 && ends2.get(i2) > starts1.get(i1+1))
+                    break;
+            }
+        }
+
+        result.setIntervalsStart(result_start);
+        result.setIntervalsEnd(result_end);
+        result.setIntervalScore(result_score);
+
+        return result;
+    }
 }
