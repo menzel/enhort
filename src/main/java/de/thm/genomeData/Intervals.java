230030f0c9a17b693f1bd48ef1c847fb975b7286
Michael Menzel <michael.menzel@mni.thm.de>
2016-02-18 17:01:39 +0100

    new version of m track bg model with new combine method

--- src/main/java/de/thm/genomeData/Intervals.java
+++ src/main/java/de/thm/genomeData/Intervals.java
@@ -2,6 +2,7 @@ package de.thm.genomeData;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Collection of utils for interval objects.
@@ -360,28 +361,28 @@ public class Intervals {
 
     }
 
-    public static Interval combine(List<Interval> intervals) {
+    public static Interval combine(List<Interval> intervals, Map<String, Double> score_map) {
         if(intervals.size() == 0){
             return null;
 
         } else if(intervals.size() == 1){
-            return intervals.get(0);
+            return null; // TODO method to set score for one interval
 
         } else if(intervals.size() == 2){
-            return combine(intervals.get(0), intervals.get(1));
+            return combine(intervals.get(0), intervals.get(1), score_map);
 
         }else{
             List<Interval> newList = new ArrayList<>();
             newList.addAll(intervals.subList(2, intervals.size()));
 
-            newList.add(combine(intervals.get(0), intervals.get(1)));
+            newList.add(combine(intervals.get(0), intervals.get(1), score_map));
 
-            return combine(newList);
+            return combine(newList, score_map);
         }
 
     }
 
-    public static Interval combine(Interval intv1, Interval intv2) {
+    public static Interval combine(Interval intv1, Interval intv2, Map<String, Double> score_map) {
 
         List<Long> starts1 = intv1.getIntervalsStart();
         List<Long> starts2 = intv2.getIntervalsStart();
@@ -391,6 +392,9 @@ public class Intervals {
 
         List<Double> scores1 = intv1.getIntervalScore();
         List<Double> scores2 = intv2.getIntervalScore();
+
+        int name1 = intv1.getUid();
+        int name2 = intv2.getUid();
 
         Interval result = new Interval();
         result.setType(intv1.getType());
@@ -419,26 +423,36 @@ public class Intervals {
                 if(ends1.get(i1) < starts2.get(i2)) { //do not overlap
                     result_start.add(starts1.get(i1));
                     result_end.add(ends1.get(i1));
-                    result_score.add(scores1.get(i1));
-                    i1++;
+                    result_score.add(score_map.get("|" + name1 + "|"));
 
-                    if(ends2.get(i2) < starts1.get(i1)) { //interval on second before next from s1
+                    i1++;
+                    if(i1 >= starts1.size()-1)
+                        System.out.println("foo");
+
+                    if(i1 >= starts1.size()-1 || ends2.get(i2) < starts1.get(i1)) { //interval on second before next from s1
                         result_start.add(starts2.get(i2));
                         result_end.add(ends2.get(i2));
                         result_score.add(scores2.get(i2));
+                        result_score.add(score_map.get("|" + name2 + "|"));
                         i2++;
                     }
 
+
                 } else if(ends2.get(i2) > ends1.get(i1)){ //overlap
+                    //first part
                     result_start.add(starts1.get(i1));
                     result_end.add(starts2.get(i2));
-                    result_score.add(scores1.get(i1));
+                    result_score.add(score_map.get("|" + name1 + "|"));
 
-                    // do not assign overlapping part
+                    //overlapping part
+                    result_start.add(starts2.get(i2));
+                    result_end.add(ends1.get(i1));
+                    result_score.add(score_map.get("|" + name1 + "|" + name2));
 
+                    //second part
                     result_start.add(ends1.get(i1));
                     result_end.add(ends2.get(i2));
-                    result_score.add(scores2.get(i2));
+                    result_score.add(score_map.get("|" + name2 + "|"));
                     i1++;
 
                 }  else { //second interval is inside the first
@@ -446,11 +460,16 @@ public class Intervals {
 
                     result_start.add(starts1.get(i1));
                     result_end.add(starts2.get(i2));
-                    result_score.add(scores1.get(i1));
+                    result_score.add(score_map.get("|" + name1 + "|"));
+
+                    //overlapping part
+                    result_start.add(starts2.get(i2));
+                    result_end.add(ends2.get(i2));
+                    result_score.add(score_map.get("|" + name1 + "|" + name2));
 
                     result_start.add(ends2.get(i2));
                     result_end.add(ends1.get(i1));
-                    result_score.add(scores1.get(i1));
+                    result_score.add(score_map.get("|" + name2 + "|"));
                 }
 
 
--- src/main/java/de/thm/mTrackBg/ScoreMultiTrackBackgroundModel.java
+++ src/main/java/de/thm/mTrackBg/ScoreMultiTrackBackgroundModel.java
@@ -1,93 +1,98 @@
 package de.thm.mTrackBg;
 
 import de.thm.backgroundModel.BackgroundModel;
-import de.thm.backgroundModel.SingleTrackBackgroundModel;
 import de.thm.genomeData.Interval;
 import de.thm.genomeData.Intervals;
 import de.thm.positionData.Sites;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Michael Menzel on 17/2/16.
  */
-public class ScoreMultiTrackBackgroundModel extends BackgroundModel{
+public class ScoreMultiTrackBackgroundModel extends BackgroundModel {
 
-    public ScoreMultiTrackBackgroundModel(Sites sites, List<Interval> intervals){
-        AppearanceTable appearanceTable = new AppearanceTable();
-        appearanceTable.fillTable(intervals,sites);
-
-        positions.addAll(generatePositions(appearanceTable, intervals));
-        this.hash = positions.hashCode();
-    }
-
-    private Collection<Long> generatePositions(AppearanceTable appearanceTable, List<Interval> intervals) {
-
-        Map<String, Interval> probIntervals = new HashMap<>();
-
-        for(String app: appearanceTable.getKeySet()){
-            if(app.compareTo("[]") == 0){ //TODO handle outside params
-                continue;
-            }
-
-            Map<Double, Integer> scoreToCount = appearanceTable.get(app);
-
-            List<Interval> currentIntervals = appearanceTable.translate(app, intervals);
-            List<Interval> negativeIntervals = appearanceTable.translateNegative(intervals, app);
-
-            for(double score: scoreToCount.keySet()){
-
-                //create intervals which only contain the current score
-                currentIntervals = currentIntervals.stream().map(i -> Intervals.subsetScore(i, score)).collect(Collectors.toList());
-                negativeIntervals = negativeIntervals.stream().map(i -> Intervals.subsetScore(i, score)).collect(Collectors.toList());
-
-                //combine intervals, substract others
-                currentIntervals.addAll(negativeIntervals.stream().map(Interval::invert).collect(Collectors.toList()));
-
-                Interval interval = Intervals.intersect(currentIntervals);
-
-                double prob = appearanceTable.getProb(app, score);
-
-
-                // probability for score in all available scores
-                long count = 0;
-                for(Interval i: currentIntervals)
-                    count += i.getIntervalScore().stream().filter(j -> j.equals(score)).count();
-
-                prob /= count;
-
-                interval.setIntervalScore(prob);
-
-                probIntervals.put(app + score, interval);
-            }
-        }
-
-        //double sum = probabilityScores.size();
-        //probabilityScores = probabilityScores.stream().map(p -> p/sum).collect(Collectors.toList());
-
-        SingleTrackBackgroundModel better = new SingleTrackBackgroundModel();
-
-        Collection<Interval> tmp = new TreeMap<>(probIntervals).values();
-
-        Interval probInterval = Intervals.combine(tmp.stream().collect(Collectors.toList()));
-
-        return better.generatePositonsByProbability(probInterval, 1000);
+    public ScoreMultiTrackBackgroundModel(Sites sites, List<Interval> intervals) {
+        Interval interval = generateProbabilityInterval(sites, intervals);
 
     }
 
-    private double count(Double value, List<Double> scores) {
+    private Interval generateProbabilityInterval(Sites sites, List<Interval> intervals) {
+        Map<String, Double> map = new HashMap<>();
+        Map<Interval, Integer> indices = new HashMap<>();
+        String key = "";
 
-        int count = 0;
+        //init indices map:
+        for(Interval interval: intervals) {
+            indices.put(interval, 0);
+        }
 
-        for (int i = 0, scoresSize = scores.size(); i < scoresSize; i++) {
-            Double score = scores.get(i);
-            if (score.equals(value)) {
-                count++;
+        for(Long p : sites.getPositions()){
+            key = "";
+
+            for(Interval interval:intervals){
+
+                List<Long> intervalStart = interval.getIntervalsStart();
+                List<Long> intervalEnd = interval.getIntervalsEnd();
+
+                int i = indices.get(interval);
+                int intervalCount = intervalStart.size()-1;
+
+                while(i < intervalCount && intervalStart.get(i) <= p){
+                    i++;
+                }
+
+                if(i == 0){
+                    if(p >= intervalStart.get(i)){
+                        key += "|" + interval.getIntervalScore().get(i);
+                    }
+
+                } else if(i == intervalCount && p > intervalEnd.get(i-1)){ //last Interval and p not in previous
+                    if(p < intervalEnd.get(i) && p >= intervalStart.get(i)){
+
+                        key += "|" + interval.getIntervalScore().get(i);
+
+                    } else{
+                        key += "|";
+                        continue;
+                    }
+                }else{
+                    if(p >= intervalEnd.get(i-1)){
+                        key += "|";
+                        continue; // not inside the last interval
+
+                    }else{
+                        key += "|" + interval.getIntervalScore().get(i);
+                    }
+                }
+
+                indices.put(interval, i);
+
+            }
+
+            if(map.containsKey(key)){
+                map.put(key, map.get(key)+1);
+            } else {
+                map.put(key, 1.);
             }
         }
 
-        return count;
+        // normalize values
+        double sum = map.values().stream().mapToDouble(Double::doubleValue).sum();
+        //map.keySet().stream().map(k -> map.put(k,map.get(k)/sum));
+
+        for(String k: map.keySet())
+            map.put(k, map.get(k)/sum);
+
+        // get p for interval without sites information
+
+        // use conversion list (score -> probability to create prob interval
+
+        Interval interval = Intervals.combine(intervals, map);
+
+        return interval;
     }
 
 }
