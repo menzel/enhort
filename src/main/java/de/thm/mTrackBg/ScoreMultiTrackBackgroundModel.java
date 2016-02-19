0050cfc1c7bdf4f22bce4a283256b7755057df14
Michael Menzel <michael.menzel@mni.thm.de>
2016-02-19 22:11:11 +0100

    refactored for testing

--- src/main/java/de/thm/mTrackBg/ScoreMultiTrackBackgroundModel.java
+++ src/main/java/de/thm/mTrackBg/ScoreMultiTrackBackgroundModel.java
@@ -6,10 +6,7 @@ import de.thm.genomeData.Interval;
 import de.thm.genomeData.Intervals;
 import de.thm.positionData.Sites;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Michael Menzel on 17/2/16.
@@ -26,8 +23,81 @@ public class ScoreMultiTrackBackgroundModel extends BackgroundModel {
 
     }
 
+    public ScoreMultiTrackBackgroundModel() { }
+
     private Interval generateProbabilityInterval(Sites sites, List<Interval> intervals) {
-        Map<String, Double> map = new HashMap<>();
+
+        Map<String, Double> map = fillOccurenceMap(intervals,sites);
+
+        // normalize values
+        double sum = sites.getPositionCount();
+
+        //substract all points that are outside
+        //sum -= map.get("||");
+        //sum -= map.get("|");
+
+        for(String k: map.keySet())
+            map.put(k, map.get(k)/sum);
+
+
+        //if( 1 == Double.compare(map.values().stream().filter(Double::isFinite).mapToDouble(i -> i).sum(), 1)) System.err.println("Not 1");
+
+        // get p for interval without sites information
+
+        Interval interval = Intervals.combine(intervals, map);
+
+        //divide each value in the interval by the count of occurences of intervals with the same key
+        //" split the probability to the intervals "
+
+
+        Map<String, Integer> occurenceTable = new HashMap<>();
+
+        //count occurences:
+        for(String key: interval.getIntervalName()){
+            if(occurenceTable.containsKey(key)){
+                occurenceTable.put(key, occurenceTable.get(key)+1);
+            } else {
+                occurenceTable.put(key, 1);
+            }
+        }
+        List<String> keys = interval.getIntervalName();
+
+        List<Double> intervalScore = interval.getIntervalScore();
+        List<Double> newScores = new ArrayList<>();
+
+        for (int i = 0; i < intervalScore.size(); i++) {
+            Double p = intervalScore.get(i);
+            int o = occurenceTable.get(keys.get(i));
+
+            if(p == null){
+                newScores.add(0d);
+
+            } else {
+                p = p / o;
+                newScores.add(p);
+            }
+        }
+
+        interval.setIntervalScore(newScores);
+        System.out.println(interval.getIntervalScore().stream().mapToDouble(i -> i).sum()); //should be 1
+
+
+        return interval;
+    }
+
+    private Double divideByProb(String key, Double value, Interval probInterval) {
+        //count how often the key (|value|value) is inside the names of the interval
+
+        double prob = probInterval.getIntervalName().stream().filter(i -> i.equals(key)).count();
+
+
+        //divide value by that count and return
+        return value/ prob;
+    }
+
+
+    public Map<String, Double> fillOccurenceMap(List<Interval> intervals, Sites sites) {
+        Map<String, Double> map = new HashMap<>(); //holds the conversion between score and probability
         Map<Interval, Integer> indices = new HashMap<>();
 
         //init indices map:
@@ -84,58 +154,7 @@ public class ScoreMultiTrackBackgroundModel extends BackgroundModel {
                 map.put(key, 1.);
             }
         }
-
-        // normalize values
-        double sum = sites.getPositionCount();
-        for(String k: map.keySet())
-            map.put(k, map.get(k)/sum);
-
-
-        //if( 1 == Double.compare(map.values().stream().filter(Double::isFinite).mapToDouble(i -> i).sum(), 1)) System.err.println("Not 1");
-
-        // get p for interval without sites information
-
-        Interval interval = Intervals.combine(intervals, map);
-
-        //divide each value in the interval by the count of occurences of intervals with the same key
-        //" split the probability to the intervals "
-
-
-        Map<String, Integer> occurenceTable = new HashMap<>();
-
-        //count occurences:
-        for(String key: interval.getIntervalName()){
-            if(occurenceTable.containsKey(key)){
-                occurenceTable.put(key, occurenceTable.get(key)+1);
-            } else {
-                occurenceTable.put(key, 1);
-            }
-        }
-        List<String> keys = interval.getIntervalName();
-
-        List<Double> intervalScore = interval.getIntervalScore();
-
-        for (int i = 0; i < intervalScore.size(); i++) {
-            Double p = intervalScore.get(i);
-            p = p / occurenceTable.get(keys.get(i));
-            intervalScore.add(i, p);
-        }
-
-        interval.setIntervalScore(intervalScore); //TODO neccessary?
-
-
-        return interval;
+        return map;
     }
-
-    private Double divideByProb(String key, Double value, Interval probInterval) {
-        //count how often the key (|value|value) is inside the names of the interval
-
-        double prob = probInterval.getIntervalName().stream().filter(i -> i.equals(key)).count();
-
-
-        //divide value by that count and return
-        return value/ prob;
-    }
-
 
 }
