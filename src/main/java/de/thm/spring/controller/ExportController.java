9b6a07a01b51f3a56c1a446ec0fa48183f40339a
Michael Menzel <michael.menzel@mni.thm.de>
2016-02-23 14:31:10 +0100

    map position back on bg export

--- src/main/java/de/thm/spring/controller/ExportController.java
+++ src/main/java/de/thm/spring/controller/ExportController.java
@@ -1,8 +1,10 @@
 package de.thm.spring.controller;
 
+import de.thm.misc.ChromosomSizes;
 import de.thm.spring.backend.Session;
 import de.thm.spring.backend.Sessions;
 import de.thm.spring.backend.StatisticsCollector;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Michael Menzel on 15/2/16.
@@ -32,6 +36,37 @@ public class ExportController {
         try(BufferedWriter writer = Files.newBufferedWriter(output.toPath())){
             output.createNewFile();
             writer.write(currentSession.getCollector().getCsv());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new FileSystemResource(output);
+    }
+
+
+
+    @RequestMapping(value="/export/bg", method= RequestMethod.GET)
+    @ResponseBody
+    public FileSystemResource exportBgSites(HttpSession httpSession) {
+        Session currentSession = Sessions.getInstance().getSession(httpSession.getId());
+        StatisticsCollector.getInstance().addDownloadC();
+        ChromosomSizes chromosomSizes = ChromosomSizes.getInstance();
+
+        List<String> positions = new ArrayList<>();
+
+        for(Long pos: currentSession.getCollector().getBackgroundSites().getPositions()){
+            Pair<String, Long> p = chromosomSizes.mapToChr(pos);
+            positions.add(p.getLeft() + "\t" + p.getRight() + "\t" + p.getRight()+1 + "\n");
+        }
+
+        //create file
+        File output = new File("/tmp/bg_output_" + httpSession.getId());
+
+        try(BufferedWriter writer = Files.newBufferedWriter(output.toPath())){
+            output.createNewFile();
+
+            for(String line: positions) writer.write(line);
 
         } catch (IOException e) {
             e.printStackTrace();
