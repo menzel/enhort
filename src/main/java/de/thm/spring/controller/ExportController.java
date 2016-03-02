b4eca53220fb68f9f4dc83ab2ebdb61f8d8ef0fe
Michael Menzel <michael.menzel@mni.thm.de>
2016-03-02 10:55:04 +0100

    todo

--- src/main/java/de/thm/spring/controller/ExportController.java
+++ src/main/java/de/thm/spring/controller/ExportController.java
@@ -55,6 +55,7 @@ public class ExportController {
 
         List<String> positions = new ArrayList<>();
 
+        //TODO check null pointer exp where there is no bg
         for (Long pos : currentSession.getCollector().getBackgroundSites().getPositions()) {
             Pair<String, Long> p = chromosomSizes.mapToChr(pos);
             positions.add(p.getLeft() + "\t" + p.getRight() + "\t" + p.getRight() + 1 + "\n");
