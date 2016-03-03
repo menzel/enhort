d210c3340c028143a0a07e20ed3364345f8178e6
Michael Menzel <michael.menzel@mni.thm.de>
2016-03-03 15:55:09 +0100

    added preprocessing for named intervals

--- src/main/java/de/thm/genomeData/NamedTrack.java
+++ src/main/java/de/thm/genomeData/NamedTrack.java
@@ -9,20 +9,64 @@ import java.util.List;
 public class NamedTrack extends Track {
 
     private final int uid = ++UID;
-    private final List<Long> intervalsStart;
-    private final List<Long> intervalsEnd;
-    private final List<String> intervalName;
     private final String name;
     private final String description;
+    private List<Long> intervalsStart;
+    private List<Long> intervalsEnd;
+    private List<String> intervalName;
 
 
     NamedTrack(List<Long> starts, List<Long> ends, List<String> names, String name, String description) {
 
-        intervalsStart = starts;
-        intervalsEnd = ends;
-        intervalName = names;
+        this.intervalsStart= starts;
+        this.intervalsEnd= ends;
+        this.intervalName= names;
+
+
         this.description = description;
         this.name = name;
+
+        preprocess();
+    }
+
+    private void preprocess() {
+        List<Long> newStart = new ArrayList<>();
+        List<Long> newEnd = new ArrayList<>();
+        List<String> newNames = new ArrayList<>();
+
+
+        if (intervalsStart.isEmpty()) return;
+
+        long start = intervalsStart.get(0);
+        long end = intervalsEnd.get(0);
+        String name = "";
+
+
+        for (int i = 0; i < intervalsStart.size(); i++) {
+
+            if (i < intervalsStart.size() - 1 && end > intervalsStart.get(i + 1)) { // overlap
+
+                if (end < intervalsEnd.get(i + 1))
+                    end = intervalsEnd.get(i + 1);
+                name = intervalName.get(i) + "_" + name;
+
+            } else {  //do not overlap
+                newStart.add(start);
+                newEnd.add(end);
+                newNames.add(intervalName.get(i) + "_" + name);
+
+                if (i >= intervalsStart.size() - 1) break; // do not get next points if this was the last
+
+                start = intervalsStart.get(i + 1);
+                end = intervalsEnd.get(i + 1);
+            }
+        }
+
+        this.intervalsStart = newStart;
+        intervalsEnd = newEnd;
+        intervalName = newNames;
+
+
     }
 
     @Override
