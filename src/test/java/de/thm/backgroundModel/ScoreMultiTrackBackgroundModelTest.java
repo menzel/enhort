c1057e2b95830370d5e3b33fd9629ff0dcc6b68e
Michael Menzel <michael.menzel@mni.thm.de>
2016-05-20 12:40:33 +0200

    test combine for list

--- src/test/java/de/thm/backgroundModel/ScoreMultiTrackBackgroundModelTest.java
+++ src/test/java/de/thm/backgroundModel/ScoreMultiTrackBackgroundModelTest.java
@@ -4,6 +4,7 @@ import de.thm.calc.Intersect;
 import de.thm.calc.TestTrackResult;
 import de.thm.genomeData.ScoredTrack;
 import de.thm.genomeData.TrackFactory;
+import de.thm.misc.ChromosomSizes;
 import de.thm.positionData.Sites;
 import org.junit.Test;
 
@@ -18,6 +19,87 @@ import static org.junit.Assert.assertEquals;
  * Created by Michael Menzel on 19/2/16.
  */
 public class ScoreMultiTrackBackgroundModelTest {
+
+    @Test
+    public void combine() throws Exception {
+        List<Long> start1 = new ArrayList<>();
+        List<Long> start2 = new ArrayList<>();
+
+        List<Long> end1 = new ArrayList<>();
+        List<Long> end2 = new ArrayList<>();
+
+        start1.add(1L);
+        start1.add(20L);
+        start1.add(50L);
+
+        end1.add(10L);
+        end1.add(30L);
+        end1.add(60L);
+
+        start2.add(5L);
+        start2.add(35L);
+        start2.add(50L);
+
+        end2.add(15L);
+        end2.add(40L);
+        end2.add(60L);
+
+        List<Double> scores1 = new ArrayList<>();
+        List<Double> scores2 = new ArrayList<>();
+
+        scores1.add(0.5);
+        scores1.add(0.2);
+        scores1.add(0.7);
+
+        scores2.add(0.4);
+        scores2.add(0.6);
+        scores2.add(0.8);
+
+        ScoredTrack interval1 = mockInterval(start1, end1, null, scores1);
+        ScoredTrack interval2 = mockInterval(start2, end2, null, scores2);
+
+
+        List<ScoredTrack> tracks = new ArrayList<>();
+        tracks.add(interval1);
+        tracks.add(interval2);
+
+
+
+        ScoreMultiTrackBackgroundModel m =  new ScoreMultiTrackBackgroundModel();
+
+        ScoredTrack result = m.combine(tracks);
+        List<Long> start = new ArrayList<>();
+        List<Long> end = new ArrayList<>();
+
+        start.add(0L);
+        start.add(1L);
+        start.add(5L);
+        start.add(10L);
+        start.add(15L);
+        start.add(20L);
+        start.add(30L);
+        start.add(35L);
+        start.add(40L);
+        start.add(50L);
+        start.add(60L);
+
+        end.add(1L);
+        end.add(5L);
+        end.add(10L);
+        end.add(15L);
+        end.add(20L);
+        end.add(30L);
+        end.add(35L);
+        end.add(40L);
+        end.add(50L);
+        end.add(60L);
+        end.add(ChromosomSizes.getInstance().getGenomeSize());
+
+
+        assertEquals(start,result.getIntervalsStart());
+        assertEquals(end,result.getIntervalsEnd());
+
+    }
 
     @Test
     public void testBackgroundModel() throws Exception {
