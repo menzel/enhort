a8a9db13fcf79ce5da4f9921672d2dd062d2171f
Michael Menzel <michael.menzel@mni.thm.de>
2016-08-26 14:18:41 +0200

    comments

--- src/main/java/de/thm/exception/CovariantsException.java
+++ src/main/java/de/thm/exception/CovariantsException.java
@@ -2,6 +2,8 @@ package de.thm.exception;
 
 
 /**
+ * Exception to handle covariants limits
+ *
  * Created by Michael Menzel on 15/2/16.
  */
 public final class CovariantsException extends Exception {
--- src/main/java/de/thm/exception/IntervalTypeNotAllowedExcpetion.java
+++ src/main/java/de/thm/exception/IntervalTypeNotAllowedExcpetion.java
@@ -1,6 +1,8 @@
 package de.thm.exception;
 
 /**
+ * Exception to handle invalid track types for bg models which only allow certain tracks
+ *
  * Created by Michael Menzel on 15/2/16.
  */
 public final class IntervalTypeNotAllowedExcpetion extends Throwable {
