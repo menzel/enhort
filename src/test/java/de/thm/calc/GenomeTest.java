dac56ad21d674088834260dab0fdb65d44b90d08
Michael Menzel <michael.menzel@mni.thm.de>
2016-11-11 11:05:38 +0100

    using count
    asserts

--- src/test/java/de/thm/calc/GenomeTest.java
+++ src/test/java/de/thm/calc/GenomeTest.java
@@ -1,5 +1,7 @@
 package de.thm.calc;
 
+import de.thm.genomeData.Track;
+import de.thm.misc.LogoCreator;
 import de.thm.positionData.Sites;
 import de.thm.positionData.UserData;
 import org.junit.Test;
@@ -8,6 +10,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Created by menzel on 11/8/16.
@@ -18,19 +22,21 @@ public class GenomeTest {
     @Test
     public void getSequence_another() throws Exception {
 
-        Genome genome = new Genome(new File("/home/menzel/Desktop/chromosomes").toPath());
-        Sites userDat = new UserData(new File("/home/menzel/Desktop/THM/lfba/enhort/sleeping_beauty.hg19.bed").toPath());
+        GenomeFactory genome = GenomeFactory.getInstance(); //new Genome(new File("/home/menzel/Desktop/chromosomes").toPath());
+        //Sites userDat = new UserData(new File("/home/menzel/Desktop/THM/lfba/enhort/sleeping_beauty.hg19.bed").toPath());
+        Sites userDat = new UserData(new File("/home/menzel/Desktop/THM/lfba/enhort/HIV-hg19.bed").toPath());
 
-        List<String> seq = genome.getSequence(userDat, 4);
+        List<String> seq = genome.getSequence(Track.Assembly.hg19, userDat, 8, Integer.MAX_VALUE);
 
-        seq.forEach(System.out::println);
+        //seq.forEach(System.out::println);
+        System.out.println(LogoCreator.createLogo(seq));
 
     }
 
     @Test
     public void getSequence() throws Exception {
 
-        Genome genome = new Genome(new File("/home/menzel/Desktop/chromosomes").toPath());
+        GenomeFactory genome = GenomeFactory.getInstance();
 
           Sites sites =  new Sites() {
              @Override
@@ -41,14 +47,20 @@ public class GenomeTest {
              @Override
             public List<Long> getPositions() {
 
-                List<Long> sites = new ArrayList<>();
+                 List<Long> sites = new ArrayList<>();
+                 /*
+                 sites.add(14L);
+                 sites.add(98L);
+                 sites.add(102L);
+                */
 
-                sites.add(120L);
-                sites.add(4200L);
-                sites.add(46000L);
-                sites.add(530000L);
-                sites.add(5400000L);
-                sites.add(60000000L);
+                // https://genome.ucsc.edu/cgi-bin/das/hg19/dna?segment=chr1:817942,817946
+                sites.add(10002L); // nta|acc
+                sites.add(817944L); // ca|taa
+                sites.add(820981L); // tta
+                sites.add(943737L);
+                sites.add(987788L); // tgtat
+
 
                 return sites;
 
@@ -65,7 +77,16 @@ public class GenomeTest {
              }
          };
 
-        genome.getSequence(sites, 5);
+        List<String> seq = genome.getSequence(Track.Assembly.hg19, sites, 5, Integer.MAX_VALUE);
+
+        //seq.forEach(System.out::println);
+
+        assertTrue(seq.get(0).contains("taac"));
+        assertTrue(seq.get(1).contains("ATAA"));
+        assertTrue(seq.get(2).contains("tta"));
+        assertTrue(seq.get(3).contains("tatg"));
+        assertTrue(seq.get(4).contains("gtat"));
+
 
     }
 
