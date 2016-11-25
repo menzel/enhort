29beebc863df072dc91d4d8aac3bf9774618f22f
Michael Menzel <michael.menzel@mni.thm.de>
2016-11-25 11:38:53 +0100

    Using Assembly # everywhere

--- src/main/java/de/thm/misc/ChromosomSizes.java
+++ src/main/java/de/thm/misc/ChromosomSizes.java
@@ -1,5 +1,6 @@
 package de.thm.misc;
 
+import de.thm.logo.GenomeFactory;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -15,50 +16,55 @@ import java.util.Map;
 public final class ChromosomSizes {
 
     private static ChromosomSizes instance;
-    private final Map<String, Integer> sizes;
-    private final List<String> names = new ArrayList<>();
+    private final Map<GenomeFactory.Assembly, Map<String, Integer>> chromosomeSizes = new HashMap<>();
+    private final Map<GenomeFactory.Assembly, List<String>> names = new HashMap<> ();
     private final Map<String, Long> offsets = new HashMap<>();
-    private long genomeSize = 0;
+    private Map<String, Long> genomeSize = new HashMap<>();
 
     /**
      * Private Constructor
      */
     private ChromosomSizes() {
 
-        sizes = new HashMap<>();
+        Map<String, Integer> hg19 = new HashMap<>();
 
-        sizes.put("chr1", 249250621);
-        sizes.put("chr2", 243199373);
-        sizes.put("chr3", 198022430);
-        sizes.put("chr4", 191154276);
-        sizes.put("chr5", 180915260);
-        sizes.put("chr6", 171115067);
-        sizes.put("chr7", 159138663);
-        sizes.put("chrX", 155270560);
-        sizes.put("chr8", 146364022);
-        sizes.put("chr9", 141213431);
-        sizes.put("chr10", 135534747);
-        sizes.put("chr11", 135006516);
-        sizes.put("chr12", 133851895);
-        sizes.put("chr13", 115169878);
-        sizes.put("chr14", 107349540);
-        sizes.put("chr15", 102531392);
-        sizes.put("chr16", 90354753);
-        sizes.put("chr17", 81195210);
-        sizes.put("chr18", 78077248);
-        sizes.put("chr20", 63025520);
-        sizes.put("chrY", 59373566);
-        sizes.put("chr19", 59128983);
-        sizes.put("chr22", 51304566);
-        sizes.put("chr21", 48129895);
+        hg19.put("chr1", 249250621);
+        hg19.put("chr2", 243199373);
+        hg19.put("chr3", 198022430);
+        hg19.put("chr4", 191154276);
+        hg19.put("chr5", 180915260);
+        hg19.put("chr6", 171115067);
+        hg19.put("chr7", 159138663);
+        hg19.put("chrX", 155270560);
+        hg19.put("chr8", 146364022);
+        hg19.put("chr9", 141213431);
+        hg19.put("chr10", 135534747);
+        hg19.put("chr11", 135006516);
+        hg19.put("chr12", 133851895);
+        hg19.put("chr13", 115169878);
+        hg19.put("chr14", 107349540);
+        hg19.put("chr15", 102531392);
+        hg19.put("chr16", 90354753);
+        hg19.put("chr17", 81195210);
+        hg19.put("chr18", 78077248);
+        hg19.put("chr20", 63025520);
+        hg19.put("chrY", 59373566);
+        hg19.put("chr19", 59128983);
+        hg19.put("chr22", 51304566);
+        hg19.put("chr21", 48129895);
 
-        names.addAll(sizes.keySet());
-        java.util.Collections.sort(names);
+        chromosomeSizes.put(GenomeFactory.Assembly.hg19, hg19);
 
+        names.put(GenomeFactory.Assembly.hg19, new ArrayList<>(hg19.keySet()));
+        java.util.Collections.sort(names.get(GenomeFactory.Assembly.hg19));
 
-        for (String key : sizes.keySet()) {
-            genomeSize += sizes.get(key);
+        // get whole genome length for hg19
+        long gz = 0;
+        for (String key : hg19.keySet()) {
+            gz += hg19.get(key);
         }
+
+        genomeSize.put("hg19", gz);
     }
 
     /**
@@ -74,14 +80,14 @@ public final class ChromosomSizes {
         return instance;
     }
 
-    public Long getChrSize(String chr) {
-        return new Long(sizes.get(chr));
+    public Long getChrSize(GenomeFactory.Assembly assembly, String chr) {
+        return new Long(chromosomeSizes.get(assembly).get(chr));
 
     }
 
-    public long getGenomeSize() {
+    public long getGenomeSize(GenomeFactory.Assembly assembly) {
 
-        return genomeSize;
+        return genomeSize.get(assembly);
     }
 
     /**
@@ -90,11 +96,11 @@ public final class ChromosomSizes {
      * @param chromosomeName - name of the chromosome to get offset for
      * @return offset for new map
      */
-    public Long offset(String chromosomeName) {
+    public Long offset(GenomeFactory.Assembly assembly, String chromosomeName) {
 
 
         if (!offsets.containsKey(chromosomeName)) {
-            offsets.put(chromosomeName, calcOffset(chromosomeName));
+            offsets.put(chromosomeName, calcOffset(assembly, chromosomeName));
         }
 
         return offsets.get(chromosomeName);
@@ -107,15 +113,15 @@ public final class ChromosomSizes {
      * @param chromosomeName - name of the chromosome
      * @return offset as Long
      */
-    private Long calcOffset(String chromosomeName) {
+    private Long calcOffset(GenomeFactory.Assembly assembly, String chromosomeName) {
 
         long offset = 0;
 
-        for (String name : names) {
+        for (String name : names.get(assembly)) {
             if (name.equals(chromosomeName))
                 return offset;
             else
-                offset += sizes.get(name);
+                offset += chromosomeSizes.get(assembly).get(name);
         }
 
         return null;
@@ -127,15 +133,15 @@ public final class ChromosomSizes {
      * @param position - position in genome
      * @return a pair containing chromosome name and position inside that chromosome.
      */
-    public Pair<String, Long> mapToChr(Long position) {
+    public Pair<String, Long> mapToChr(GenomeFactory.Assembly assembly, Long position) {
         int i = 0;
-        Long chrSize = Long.valueOf(sizes.get(names.get(0)));
+        Long chrSize = Long.valueOf(chromosomeSizes.get(assembly).get(names.get(assembly).get(0)));
 
         try {
 
             while (position > chrSize) {
                 position -= chrSize;
-                chrSize = Long.valueOf(sizes.get(names.get(++i)));
+                chrSize = Long.valueOf(chromosomeSizes.get(assembly).get(names.get(assembly).get(++i)));
             }
         } catch (IndexOutOfBoundsException e){
             System.err.println("iofe");
@@ -143,7 +149,7 @@ public final class ChromosomSizes {
             return null;
         }
 
-        return new ImmutablePair<>(names.get(i), position);
+        return new ImmutablePair<>(names.get(assembly).get(i), position);
     }
 
 }
