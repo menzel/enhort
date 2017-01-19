288bcde41390a2470483d32aaf7547a6d1a50568
Michael Menzel <michael.menzel@mni.thm.de>
2017-01-19 13:45:42 +0100

    Using T

--- src/main/java/de/thm/stat/Test.java
+++ src/main/java/de/thm/stat/Test.java
@@ -4,9 +4,11 @@ import de.thm.calc.TestTrackResult;
 import de.thm.genomeData.Track;
 
 /**
+ * Interface for statistical test evaluation
+ *
  * Created by Michael Menzel on 1/3/16.
  */
 public interface Test<T extends Track>{
 
-    TestResult test(TestTrackResult testTrackResultA, TestTrackResult testTrackResultB, Track track);
+    TestResult test(TestTrackResult testTrackResultA, TestTrackResult testTrackResultB, T track);
 }
