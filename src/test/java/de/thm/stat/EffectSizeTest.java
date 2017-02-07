ccbfff360216cd5524afe8ea8845d0ab87966538
Michael Menzel <michael.menzel@mni.thm.de>
2017-02-07 15:18:17 +0100

    updated test to new measurement

--- src/test/java/de/thm/stat/EffectSizeTest.java
+++ src/test/java/de/thm/stat/EffectSizeTest.java
@@ -10,13 +10,13 @@ import static org.junit.Assert.assertEquals;
 public class EffectSizeTest {
 
     @Test
-    public void testTest1() throws Exception {
+    public void testFoldChange() throws Exception {
         EffectSize fc = new EffectSize();
 
-        assertEquals(1.2, fc.foldChange(1139,5,9021,131), 0.1);
+        assertEquals(26.2, fc.foldChange(1139,5,9021,131), 0.1);
         assertEquals(Double.POSITIVE_INFINITY, fc.foldChange(0,1144,86,9066), 0.1);
-        assertEquals(0, fc.foldChange(0,0,0,0), 0.0001);
-        assertEquals(0, fc.foldChange(10,10,10,10), 0.0001);
+        assertEquals(Double.POSITIVE_INFINITY, fc.foldChange(0,0,0,0), 0.0001);
+        assertEquals(1., fc.foldChange(10,10,10,10), 0.0001);
 
     }
 }
