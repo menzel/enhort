4b5ee9dff722dc757fa5753fffc3c4503e9b2bf2
Michael Menzel <michael.menzel@mni.thm.de>
2017-02-07 15:18:01 +0100

    comments

--- src/main/java/de/thm/stat/EffectSize.java
+++ src/main/java/de/thm/stat/EffectSize.java
@@ -22,17 +22,28 @@ public final class EffectSize {
      */
     public double test(TestTrackResult testTrackResultA, TestTrackResult testTrackResultB) {
 
-
         return foldChange(testTrackResultA.getIn(), testTrackResultA.getOut(), testTrackResultB.getIn(), testTrackResultB.getOut());
-
     }
 
+
+    /**
+     * Calculates the fold change of given in out values for two given sets by the following formula:
+     * The maximum of ( maximum inside / minimum inside and maximum outside / maximum outside)
+     * This inverts values smaller than 1 and returns the max fold change for in or outside counts
+     *
+     * @param in - count inside of first set
+     * @param out -  count outside of first set
+     * @param in1 - count insideof second set
+     * @param out1 - count outside of second set
+     *
+     * @return fold change
+     */
     double foldChange(double in, double out, double in1, double out1) {
 
         if(in1 == 0 || out1 == 0 || out  == 0 || in == 0)
             return Double.POSITIVE_INFINITY;
 
-        // the maximum of ( maximum inside / minimum inside and maximum outside / maximum outside) -- inverts
+        // the maximum of ( maximum inside / minimum inside and maximum outside / maximum outside) -- inverts values smaller than 1
         return max(max(in,in1) / min(in,in1), max(out,out1) / min(out,out1));
     }
 }
