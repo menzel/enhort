0bdbbfd0d8df8eff01614b6b203b2353c5e17f19
Michael Menzel <michael.menzel@mni.thm.de>
2017-02-13 15:24:01 +0100

    factoryFactory in precalc

---
+++ src/main/java/de/thm/precalc/SiteFactoryFactory.java
@@ -0,0 +1,40 @@
+package de.thm.precalc;
+
+import de.thm.logo.GenomeFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Factory for PrecalcSitesFactory to store factory for different genomes
+ *
+ * Created by menzel on 2/13/17.
+ */
+public class SiteFactoryFactory {
+    private static SiteFactoryFactory instance;
+    private Map<GenomeFactory.Assembly, SiteFactory> factories;
+
+    private SiteFactoryFactory(){
+        factories = new HashMap<>();
+        factories.put(GenomeFactory.Assembly.hg19, new SiteFactory(GenomeFactory.Assembly.hg19, 100000));
+    }
+
+    public static SiteFactoryFactory getInstance(){
+
+        if (instance == null) {
+            instance = new SiteFactoryFactory();
+        }
+        return instance;
+    }
+
+    /**
+     * Returns a factory by given assembly
+     *
+     * @param assembly nr to get
+     *
+     * @return factory of given assembly
+     */
+    public SiteFactory get(GenomeFactory.Assembly assembly) {
+        return this.factories.get(assembly);
+    }
+}
