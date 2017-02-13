bf0fa52ca90f86581f3872124b2f3a54a09036c8
Michael Menzel <michael.menzel@mni.thm.de>
2017-02-13 15:22:58 +0100

    bg model for precalc sites

---
+++ src/main/java/de/thm/precalc/PrecalcBackgroundModel.java
@@ -0,0 +1,48 @@
+package de.thm.precalc;
+
+import de.thm.logo.GenomeFactory;
+import de.thm.positionData.Sites;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * BackgroundModel sites object for precalc sites
+ *
+ * Created by menzel on 2/13/17.
+ */
+public class PrecalcBackgroundModel implements Sites {
+
+    private final GenomeFactory.Assembly assembly;
+    private List<Long> positions;
+
+    PrecalcBackgroundModel(GenomeFactory.Assembly assembly, List<Long> positions) {
+        this.assembly = assembly;
+        this.positions = positions;
+    }
+
+    @Override
+    public void addPositions(Collection<Long> values) {
+        this.positions.addAll(values);
+    }
+
+    @Override
+    public List<Long> getPositions() {
+        return this.positions;
+    }
+
+    @Override
+    public void setPositions(List<Long> positions) {
+        this.positions = positions;
+    }
+
+    @Override
+    public int getPositionCount() {
+        return positions.size();
+    }
+
+    @Override
+    public GenomeFactory.Assembly getAssembly() {
+        return this.assembly;
+    }
+}
