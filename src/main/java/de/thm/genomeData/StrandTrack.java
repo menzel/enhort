e32d6cb2cc68fefca53ff140cd696c5b802d841e
Michael Menzel <michael.menzel@mni.thm.de>
2017-03-06 10:43:54 +0100

    added assembly nr and cell line

--- src/main/java/de/thm/genomeData/StrandTrack.java
+++ src/main/java/de/thm/genomeData/StrandTrack.java
@@ -11,28 +11,18 @@ public class StrandTrack extends AbstractTrack {
 
     private final List<Character> strands;
 
-    StrandTrack(List<Long> starts, List<Long> ends, String name, String description, List<Character> strands) {
-        super(starts, ends, name, description);
+    StrandTrack(List<Long> starts, List<Long> ends, List<Character> strands, String name, String description, GenomeFactory.Assembly assembly, CellLine cellLine) {
+        super(starts, ends, name, description,assembly, cellLine);
         this.strands = strands;
-    }
-
-
-    @Override
-    public GenomeFactory.Assembly getAssembly() {
-        return null;
-    }
-
-    @Override
-    public CellLine getCellLine() {
-        return null;
     }
 
     @Override
     public Track clone() {
-        return null;
+        return new StrandTrack(this.getStarts(), this.getEnds(), this.getStrands(), this.getName(), this.getDescription(), this.getAssembly(), this.getCellLine());
     }
 
     public List<Character> getStrands() {
         return strands;
     }
+
 }
