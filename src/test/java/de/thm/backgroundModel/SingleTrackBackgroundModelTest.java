3245685483b49513ca000fd4f6e65c829db4b898
Michael Menzel <michael.menzel@mni.thm.de>
2017-06-21 15:31:58 +0200

    test for the single bg model

---
+++ src/test/java/de/thm/backgroundModel/SingleTrackBackgroundModelTest.java
@@ -0,0 +1,148 @@
+package de.thm.backgroundModel;
+
+import de.thm.calc.Intersect;
+import de.thm.calc.TestTrackResult;
+import de.thm.genomeData.InOutTrack;
+import de.thm.genomeData.Track;
+import de.thm.genomeData.TrackFactory;
+import de.thm.genomeData.Tracks;
+import de.thm.logo.GenomeFactory;
+import de.thm.positionData.Sites;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for the single track background model
+ * <p>
+ * Created by menzel on 6/21/17.
+ */
+public class SingleTrackBackgroundModelTest {
+    @Test
+    public void randPositions() throws Exception {
+
+        List<Long> starts = new ArrayList<>();
+        List<Long> ends = new ArrayList<>();
+
+        starts.add(5L);
+        starts.add(15L);
+        starts.add(25L);
+
+        ends.add(10L);
+        ends.add(20L);
+        ends.add(26L);
+
+        InOutTrack base = mockTrack(starts, ends);
+
+        SingleTrackBackgroundModel model = new SingleTrackBackgroundModel(GenomeFactory.Assembly.hg19);
+
+        //create random sites INSIDE the base track
+        List<Long> sites = new ArrayList<>(model.randPositions(3000, base));
+        sites = sites.stream().distinct().collect(Collectors.toList());
+
+        Intersect<InOutTrack> intersect = new Intersect<>();
+        Sites sitesObject = mock(Sites.class);
+        when(sitesObject.getPositions()).thenReturn(sites);
+
+        //intersect sites with the base track
+        TestTrackResult result = intersect.searchSingleInterval(base, sitesObject);
+
+        assertEquals(0, result.getOut()); // no sites should be outside
+        assertEquals(11, result.getIn()); // all positions should be taken, could fail if the rand pos is too small
+
+        //check if each position is choosen (could fail sometimes)
+        assertArrayEquals(sites.toArray(), new Long[]{5L, 6L, 7L, 8L, 9L, 15L, 16L, 17L, 18L, 19L, 25L});
+    }
+
+
+    @Test
+    public void randPositionsSecond() throws Exception {
+
+        int count = 300000;
+
+        List<Long> starts = new ArrayList<>();
+        List<Long> ends = new ArrayList<>();
+
+        starts.add(5L);
+        starts.add(15L);
+        starts.add(25L);
+        starts.add(30L);
+
+        ends.add(10L);
+        ends.add(20L);
+        ends.add(26L);
+        ends.add(10000L);
+
+        InOutTrack base = mockTrack(starts, ends);
+
+        SingleTrackBackgroundModel model = new SingleTrackBackgroundModel(GenomeFactory.Assembly.hg19);
+
+        //create random sites INSIDE the base track
+        List<Long> sites = new ArrayList<>(model.randPositions(count, base));
+
+        Intersect<InOutTrack> intersect = new Intersect<>();
+        Sites sitesObject = mock(Sites.class);
+        when(sitesObject.getPositions()).thenReturn(sites);
+
+        //intersect sites with the base track
+        TestTrackResult result = intersect.searchSingleInterval(base, sitesObject);
+
+        assertEquals(0, result.getOut()); // no sites should be outside
+        assertEquals(count, result.getIn()); // all positions should be somewhere inside
+    }
+
+
+    private InOutTrack mockTrack(List<Long> start, List<Long> end) {
+
+        return TrackFactory.getInstance().createInOutTrack(start, end, "name", "desc", GenomeFactory.Assembly.hg19);
+    }
+
+
+    @Test
+    public void randPosReal() throws Exception {
+
+        TrackFactory.getInstance().loadTracks();
+        Track contigs = TrackFactory.getInstance().getTrackByName("Contigs", GenomeFactory.Assembly.hg19);
+
+        int count = 300000;
+
+        List<Long> starts = new ArrayList<>();
+        List<Long> ends = new ArrayList<>();
+
+        starts.add(5L);
+        starts.add(10000L);
+        starts.add(25000L);
+        starts.add(3000000L);
+
+        ends.add(10L);
+        ends.add(20000L);
+        ends.add(26000L);
+        ends.add(4000000L);
+
+        InOutTrack base = mockTrack(starts, ends);
+
+        SingleTrackBackgroundModel model = new SingleTrackBackgroundModel(GenomeFactory.Assembly.hg19);
+
+
+        Track filteredInvertTrack = Tracks.intersect(Tracks.invert(base), contigs);
+
+        //create random sites INSIDE the base track
+        List<Long> sites = new ArrayList<>(model.randPositions(count, filteredInvertTrack));
+
+        Intersect<InOutTrack> intersect = new Intersect<>();
+        Sites sitesObject = mock(Sites.class);
+        when(sitesObject.getPositions()).thenReturn(sites);
+
+        //intersect sites with the base track
+        TestTrackResult result = intersect.searchSingleInterval((InOutTrack) contigs, sitesObject);
+        assertEquals(0, result.getOut()); // no sites should be outside the contigs
+        assertEquals(count, result.getIn()); // all positions should be somewhere inside the contigs
+    }
+}
