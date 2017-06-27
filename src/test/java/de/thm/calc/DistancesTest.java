ef6c85fc3e733102298ce70a40878b7a1b28a159
Michael Menzel <michael.menzel@mni.thm.de>
2017-06-27 13:42:37 +0200

    mockito cleanup

--- src/test/java/de/thm/calc/DistancesTest.java
+++ src/test/java/de/thm/calc/DistancesTest.java
@@ -6,11 +6,12 @@ import de.thm.logo.GenomeFactory;
 import de.thm.positionData.Sites;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test Distance calc
@@ -21,55 +22,14 @@ public class DistancesTest {
     @Test
     public void searchTrack() throws Exception {
 
-        //create data
-        ArrayList<Long> startList = new ArrayList<>();
+        DistanceTrack track = TrackFactory.getInstance().createDistanceTrack(Arrays.asList(5L,10L,20L), "testtrack", "no desc", GenomeFactory.Assembly.hg19);
 
-        startList.add(5L);
-        startList.add(10L);
-        startList.add(20L);
+        Sites sites = mock(Sites.class);
+        List<Long> r = Arrays.asList(1L,7L,10L,12L,19L,25L);
 
-        DistanceTrack track = TrackFactory.getInstance().createDistanceTrack(startList, "testtrack", "no desc", GenomeFactory.Assembly.hg19);
-
-        Sites sites =  new Sites() {
-
-            @Override
-            public void addPositions(Collection<Long> values) {}
-
-            @Override
-            public List<Long> getPositions() {
-
-                List<Long> sites = new ArrayList<>();
-
-                sites.add(1L);
-                sites.add(7L);
-                sites.add(10L);
-                sites.add(12L);
-                sites.add(19L);
-                sites.add(25L);
-
-                return sites;
-
-            }
-
-            @Override
-            public void setPositions(List<Long> positions) {}
-
-            @Override
-            public List<Character> getStrands() {
-                return null;
-            }
-
-            @Override
-            public int getPositionCount() {
-                return 0;
-            }
-
-            @Override
-            public GenomeFactory.Assembly getAssembly() {
-                return GenomeFactory.Assembly.hg19;
-            }
-        };
-
+        when(sites.getPositions()).thenReturn(r);
+        when(sites.getAssembly()).thenReturn(GenomeFactory.Assembly.hg19);
+        when(sites.getPositionCount()).thenReturn(r.size());
 
         // run distances
         Distances distances = new Distances();
@@ -77,19 +37,7 @@ public class DistancesTest {
 
         // create expected data
 
-        ArrayList<Double> expected = new ArrayList<>();
-
-        expected.add(4.);
-        expected.add(2.);
-        expected.add(0.);
-        expected.add(2.);
-        expected.add(-1.);
-        expected.add(5.);
-
-        assertEquals(expected, result.getResultScores());
-
-
-
+        assertEquals(Arrays.asList(4.,2.,0.,2.,-1.,5.), result.getResultScores());
     }
 
 }
