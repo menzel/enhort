1a02c02aa6f5427d7b044ce2b45beeb0a1dcfc2b
Michael Menzel <michael.menzel@mni.thm.de>
2017-07-24 12:56:19 +0200

    wizard controller, logo and html

---
+++ src/main/java/de/thm/spring/controller/WizardController.java
@@ -0,0 +1,120 @@
+package de.thm.spring.controller;
+
+import de.thm.exception.CovariantsException;
+import de.thm.exception.NoTracksLeftException;
+import de.thm.logo.GenomeFactory;
+import de.thm.positionData.UserData;
+import de.thm.spring.backend.BackendConnector;
+import de.thm.spring.backend.Session;
+import de.thm.spring.backend.Sessions;
+import de.thm.spring.command.BackendCommand;
+import de.thm.spring.command.InterfaceCommand;
+import de.thm.stat.ResultCollector;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.nio.file.Path;
+import java.util.UUID;
+
+
+@Controller
+public class WizardController {
+
+    private static final Path basePath = new File("/tmp").toPath();
+
+
+    @RequestMapping(value = "/wiz", method = RequestMethod.GET)
+    public String wizard(Model model, HttpSession httpSession){
+
+        model.addAttribute("page", "upload");
+
+        InterfaceCommand command = new InterfaceCommand();
+        model.addAttribute("interfaceCommand", command);
+
+        return "wizard";
+    }
+
+
+    @RequestMapping(value = "/wiz", method = RequestMethod.POST)
+    public String wizard2(Model model, HttpSession httpSession, @RequestParam("file") MultipartFile file, @ModelAttribute InterfaceCommand interfaceCommand){
+
+        Sessions sessionsControll = Sessions.getInstance();
+        Session currentSession = sessionsControll.getSession(httpSession.getId());
+
+        if(currentSession.getSites() == null) {
+
+            String name = file.getOriginalFilename();
+            String uuid = name + "-" + UUID.randomUUID();
+
+            Path inputFilepath = null;
+
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(basePath.resolve(uuid).toFile()));
+                    stream.write(bytes);
+                    stream.close();
+
+                    inputFilepath = basePath.resolve(uuid);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            UserData data = null;
+
+            if (inputFilepath != null) {
+                data = new UserData(GenomeFactory.Assembly.valueOf(interfaceCommand.getAssembly()), inputFilepath);
+                currentSession.setSites(data);
+            }
+
+            BackendCommand command = new BackendCommand(data);
+
+            ResultCollector collector = null;
+            try {
+                collector = BackendConnector.getInstance().runAnalysis(command);
+            } catch (CovariantsException | SocketTimeoutException | NoTracksLeftException e) {
+                e.printStackTrace();
+            }
+
+            if (collector == null) {
+                model.addAttribute("errorMessage", "Backend Connection Error");
+                return "error";
+            }
+
+            currentSession.setCollector(collector);
+
+            model.addAttribute("interfaceCommand", interfaceCommand);
+            model.addAttribute("trackPackages", collector.getKnownPackages());
+            model.addAttribute("celllines", collector.getKnownCelllines());
+
+            model.addAttribute("page", "packages");
+            return "wizard";
+
+        } else if(interfaceCommand.getPackageNames().size() > 0) {
+
+            interfaceCommand.setAssembly(currentSession.getSites().getAssembly().toString());
+            model.addAttribute("interfaceCommand", interfaceCommand);
+            model.addAttribute("tracks", currentSession.getCollector().getResults());
+            model.addAttribute("page", "covariates");
+
+            //TODO
+
+            return "wizard";
+
+        }
+        return "error";
+    }
+}
