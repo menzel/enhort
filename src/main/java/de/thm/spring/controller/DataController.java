0f6265f7125a232c29d84a92eab7d77004d6bc55
Michael Menzel <michael.menzel@mni.thm.de>
2017-08-07 15:33:25 +0200

    removed trackname lsit

--- src/main/java/de/thm/spring/controller/DataController.java
+++ src/main/java/de/thm/spring/controller/DataController.java
@@ -2,7 +2,6 @@ package de.thm.spring.controller;
 
 import de.thm.exception.CovariantsException;
 import de.thm.exception.NoTracksLeftException;
-import de.thm.genomeData.Track;
 import de.thm.logo.GenomeFactory;
 import de.thm.result.DataViewResult;
 import de.thm.spring.backend.BackendConnector;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.net.SocketTimeoutException;
-import java.util.stream.Collectors;
 
 @Controller
 public class DataController {
@@ -33,7 +31,6 @@ public class DataController {
             if(collector != null) {
 
                 model.addAttribute("tracks", collector.getTracks());
-                model.addAttribute("tracknames", collector.getTracks().stream().map(Track::getName).distinct().collect(Collectors.toList()));
                 model.addAttribute("assembly", collector.getAssembly());
                 model.addAttribute("celllines", CellLineCache.getInstance(collector).getCellLines());
 
