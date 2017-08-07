637b2e2296a5e555c235f269108074ff1cdceffc
Michael Menzel <michael.menzel@mni.thm.de>
2017-08-07 12:37:34 +0200

    implemented cell line cache

---
+++ src/main/java/de/thm/spring/cache/CellLineCache.java
@@ -0,0 +1,51 @@
+package de.thm.spring.cache;
+
+import de.thm.genomeData.Track;
+import de.thm.result.DataViewResult;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CellLineCache {
+
+    private static CellLineCache instance;
+    private Map<String, List<String>> newCellLinesMap;
+
+    private CellLineCache(DataViewResult collector){
+
+        this.newCellLinesMap = new HashMap<>();
+        List<String> knownCelllines = collector.getTracks().stream().map(Track::getCellLine).distinct().collect(Collectors.toList());
+        Map<String, List<String>> cellLines = collector.getCellLines();
+
+        for (String cellline : cellLines.keySet()) {
+
+            if (cellLines.get(cellline) != null) {
+                List<String> subs = cellLines.get(cellline);
+                subs = subs.stream().filter(knownCelllines::contains).collect(Collectors.toList());
+
+                if(!subs.isEmpty())
+                    newCellLinesMap.put(cellline, subs);
+
+            } else {
+                if(knownCelllines.contains(cellline))
+                    newCellLinesMap.put(cellline, null);
+            }
+        }
+    }
+
+    public static CellLineCache getInstance(DataViewResult collector){
+        if(instance == null)
+            instance = new CellLineCache(collector);
+        //TODO check if something has changed
+
+        return instance;
+    }
+
+    public Map<String, List<String>> getCellLines(){
+        return newCellLinesMap;
+    }
+
+
+}
--- src/main/java/de/thm/spring/controller/ApplicationController.java
+++ src/main/java/de/thm/spring/controller/ApplicationController.java
@@ -2,12 +2,12 @@ package de.thm.spring.controller;
 
 import de.thm.exception.CovariantsException;
 import de.thm.exception.NoTracksLeftException;
-import de.thm.genomeData.Track;
 import de.thm.logo.GenomeFactory;
 import de.thm.result.DataViewResult;
 import de.thm.spring.backend.BackendConnector;
 import de.thm.spring.backend.Sessions;
 import de.thm.spring.backend.StatisticsCollector;
+import de.thm.spring.cache.CellLineCache;
 import de.thm.spring.command.BackendCommand;
 import de.thm.spring.command.ExpressionCommand;
 import de.thm.spring.command.InterfaceCommand;
@@ -19,10 +19,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.SocketTimeoutException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Application Controller for default routing
@@ -91,29 +87,9 @@ public class ApplicationController {
             //TODO: save the list of known cell lines somewhere and only reload if needed
             if(collector != null) {
 
-                List<String> knownCelllines = collector.getTracks().stream().map(Track::getCellLine).distinct().collect(Collectors.toList());
-                Map<String, List<String>> cellLines = collector.getCellLines();
-                Map<String, List<String>> newCellLinesMap =  new HashMap<>();
-
-
-                for (String cellline : cellLines.keySet()) {
-
-                    if (cellLines.get(cellline) != null) {
-                        List<String> subs = cellLines.get(cellline);
-                        subs = subs.stream().filter(knownCelllines::contains).collect(Collectors.toList());
-
-                        if(!subs.isEmpty())
-                            newCellLinesMap.put(cellline, subs);
-
-                    } else {
-                        if(knownCelllines.contains(cellline))
-                            newCellLinesMap.put(cellline, null);
-                    }
-                }
-
                 model.addAttribute("tracks", collector.getTracks());
                 model.addAttribute("assembly", collector.getAssembly());
-                model.addAttribute("celllines", newCellLinesMap);
+                model.addAttribute("celllines", CellLineCache.getInstance(collector).getCellLines());
 
             } else {
                 System.err.println("ApplicationController: Collector for data is null");
