9d9cbc7832cd642e4376c8488b03293d8ecce0f1
Michael Menzel <michael.menzel@mni.thm.de>
2017-08-31 11:33:51 +0200

    caching tracks, and checks to reload cache

--- src/main/java/de/thm/spring/cache/DataTableCache.java
+++ src/main/java/de/thm/spring/cache/DataTableCache.java
@@ -1,5 +1,6 @@
 package de.thm.spring.cache;
 
+import de.thm.genomeData.tracks.Track;
 import de.thm.genomeData.tracks.TrackPackage;
 import de.thm.result.DataViewResult;
 
@@ -8,12 +9,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class CellLineCache {
+public class DataTableCache {
 
-    private static CellLineCache instance;
+    private static DataTableCache instance;
     private Map<String, List<String>> newCellLinesMap;
+    private static Integer lastCollectorHash;
+    private List<String> trackNames;
 
-    private CellLineCache(DataViewResult collector){
+    private DataTableCache(DataViewResult collector){
+
+
+        // Cell lines
 
         this.newCellLinesMap = new HashMap<>();
         List<String> knownCelllines = collector.getPackages().stream().map(TrackPackage::getCellLine).collect(Collectors.toList());
@@ -33,14 +39,29 @@ public class CellLineCache {
                     newCellLinesMap.put(cellline, null);
             }
         }
+
+        // track names
+
+        trackNames = collector.getPackages().stream()
+                .flatMap(trackPackage -> trackPackage.getTrackList().stream())
+                .map(Track::getName)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
     }
 
-    public static CellLineCache getInstance(DataViewResult collector){
-        if(instance == null)
-            instance = new CellLineCache(collector);
-        //TODO check if something has changed
+    public static DataTableCache getInstance(DataViewResult collector){
+        if(instance == null || ! hashCollector(collector).equals(lastCollectorHash)) {
+
+            instance = new DataTableCache(collector);
+            lastCollectorHash = hashCollector(collector);
+        }
 
         return instance;
+    }
+
+    private static Integer hashCollector(DataViewResult collector) {
+        return collector.getCellLines().size() + collector.getCellLines().size() + collector.getAssembly().hashCode();
     }
 
     public Map<String, List<String>> getCellLines(){
@@ -48,4 +69,7 @@ public class CellLineCache {
     }
 
 
+    public List<String> getTrackNames() {
+        return this.trackNames;
+    }
 }
