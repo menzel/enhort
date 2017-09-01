d16137db265f500e84593e30d6fc30d91e832f37
Michael Menzel <michael.menzel@mni.thm.de>
2017-09-01 11:16:30 +0200

    toggle all function

--- src/main/resources/static/js/celltable.js
+++ src/main/resources/static/js/celltable.js
@@ -1,8 +1,21 @@
 var rowheader = document.getElementsByClassName("rowheader");
 var colheader = document.getElementsByClassName("trackname");
 var coln = colheader.length;
-var rown = rowheader.length;
+var rown = rowheader.length - 1;
 
+
+
+/*
+Set given boolean val to all checkboxes in given row
+ */
+function checkRow(row, val) {
+
+    for(var j=1; j<=coln; j++) { // first element is the row header, skip
+        var child = row.nextElementSibling.parentElement.cells[j].firstElementChild;
+        if(child !== null)
+            child.checked = val;
+    }
+}
 
 /*Select all in row if the rowname is clicked */
 
@@ -11,31 +24,51 @@ for(var i=0, l=rowheader.length; i<l; i++){
     rowheader[i].addEventListener('click', function (event) {
         var row = event.currentTarget;
 
-        //iterate over childs and toggle checkboxes
-        for(var j=1; j<=coln; j++){ // first element is the row header, skip
+        for (var j = 1; j <= coln; j++) { // first element is the row header, skip
             var child = row.nextElementSibling.parentElement.cells[j].firstElementChild;
 
-            if(child !== null)
-                child.checked = !child.checked;
+            if (child !== null && child.checked === false) { //if any element is false:
+                checkRow(row, true); //set all checkboxes to true
+                break;
+            }
         }
+
+        if (j === coln+1) //if all elements are true, set all to false:
+            checkRow(row, false);
     });
 }
 
+/*
+Set the given val to all checkboxes with the given colnum
+ */
+function checkCol(rowheader, colnum, val) {
+
+    for(var j = 0; j <= rown; j++) { //iterate over all rows
+        var child = rowheader[j].nextElementSibling.parentElement.cells[colnum].firstElementChild; // get elements from each row
+        if(child !== null)
+            child.checked = val;
+    }
+}
+
 /*Select all in column if the colname is clicked */
-
-
 for(i=0, l=coln; i<l; i++){
     colheader[i].addEventListener('click', function (event) {
         var colnum = event.currentTarget.parentElement.cellIndex; // colnumber clicked
         col = event.currentTarget;
 
-        for(var j = 0; j <= rown; j++){ //iterate over all rows
+        for(var j = 0; j <= rown; j++) { //iterate over all rows
             child = rowheader[j].nextElementSibling.parentElement.cells[colnum].firstElementChild; // get elements from each row
 
-            if(child !== null)
-                child.checked = !child.checked;
-
+            if (child !== null && child.checked === false) {
+                checkCol(rowheader, colnum, true);
+                break; //break after all elements are set
+            }
         }
+
+        if(j === rown+1){ // if all elements are true, set all to false now:
+            checkCol(rowheader, colnum, false);
+        }
+
     });
 }
 
