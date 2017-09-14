2bb1a31256a904f4c424f773afb5c9b2046785d7
Michael Menzel <michael.menzel@mni.thm.de>
2017-09-14 12:22:40 +0200

    welcome route

--- src/main/java/de/thm/spring/controller/ApplicationController.java
+++ src/main/java/de/thm/spring/controller/ApplicationController.java
@@ -20,6 +20,12 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class ApplicationController {
 
+    @RequestMapping(value = {"/welcome", "/"}, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        return "index";
+    }
+
     @RequestMapping(value = "/clear_session", method = RequestMethod.GET)
     public String deleteSession(Model model, HttpSession session) {
 
