4db16fbc24650154b67808150be3226930a8797b
Michael Menzel <michael.menzel@mni.thm.de>
2017-11-06 15:48:38 +0100

    refactored covariants ex name

---
+++ enhortlib/src/main/java/de/thm/exception/CovariatesException.java
@@ -0,0 +1,15 @@
+package de.thm.exception;
+
+
+/**
+ * Exception to handle covariates limits
+ *
+ * Created by Michael Menzel on 15/2/16.
+ */
+public final class CovariatesException extends Exception {
+
+
+    public CovariatesException(String message) {
+        super(message);
+    }
+}
