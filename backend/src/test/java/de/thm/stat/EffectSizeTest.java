d200fcac241eca652eda42b4f36141b95291be70
Michael Menzel <michael.menzel@mni.thm.de>
2018-01-05 14:54:42 +0100

    fixed tests for log10 effect size update

--- backend/src/test/java/de/thm/stat/EffectSizeTest.java
+++ backend/src/test/java/de/thm/stat/EffectSizeTest.java
@@ -13,11 +13,11 @@ public class EffectSizeTest {
     public void testFoldChange() throws Exception {
         EffectSize fc = new EffectSize();
 
-        assertEquals(1.325, fc.foldChange(6000, 4000, 4700, 5300), 0.0001);
-        assertEquals(3.27, fc.foldChange(1139,5,9021,131), 0.1);
-        assertEquals(Double.POSITIVE_INFINITY, fc.foldChange(0,1144,86,9066), 0.1);
-        assertEquals(Float.NaN, fc.foldChange(0,0,0,0), 0.0001);
-        assertEquals(1., fc.foldChange(10,10,10,10), 0.0001);
+        assertEquals(0.122216, fc.foldChange(6000, 4000, 4700, 5300), 0.0001);
+        assertEquals(0.514548, fc.foldChange(1139, 5, 9021, 131), 0.1);
+        assertEquals(1.037, fc.foldChange(0, 1144, 86, 9066), 0.1);
+        assertEquals(0.0, fc.foldChange(0, 0, 0, 0), 0.0001);
+        assertEquals(0.0, fc.foldChange(10, 10, 10, 10), 0.0001);
 
     }
 }
