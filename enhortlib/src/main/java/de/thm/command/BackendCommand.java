8917e93768df2302804c3e3af6eb088e533ace82
Michael Menzel <michael.menzel@mni.thm.de>
2018-02-16 10:24:02 +0100

    Builder pattern for backend command

--- enhortlib/src/main/java/de/thm/command/BackendCommand.java
+++ enhortlib/src/main/java/de/thm/command/BackendCommand.java
@@ -7,9 +7,14 @@ import de.thm.positionData.Sites;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * Backend command object to send data from interface to backend. Is immutable.
+ * Backend command object to send data from interface to backend.
+ * BackendCommand is immutable.
+ *
+ * Is build using the builder pattern, example:
+ * new BackendCommand.Builder(Command.Task.ANALZYE_SINGLE, currentSession.getSites().getAssembly()).sites(sites).sitesBg(sitesBg).build();
  *
  * Created by Michael Menzel on 11/3/16.
  */
@@ -26,79 +31,129 @@ public final class BackendCommand implements Command {
     private final List<Sites> batchSites;
     private final Task task;
 
-
-    public BackendCommand(List<Sites> sites, Task task) {
-        this.covariants = new ArrayList<>();
-        this.sites = null;
-        this.minBg = sites.get(0).getPositionCount() < 10000 ? 10000 : sites.get(0).getPositionCount();
-        this.customTracks = new ArrayList<>();
-        this.assembly = sites.get(0).getAssembly();
-        this.logoCovariate = false;
-        this.createLogo = false;
-        this.sitesBg = null;
-        this.tracks = Collections.emptyList();
-        this.task = task;
-        batchSites = sites;
-
+    /**
+     * Build using the builder pattern
+     *
+     * @param builder - builder object
+     */
+    private BackendCommand(Builder builder) {
+        this.covariants = builder.covariants;
+        this.minBg = builder.minBg;
+        this.customTracks = builder.customTracks;
+        this.sites = builder.sites;
+        this.sitesBg = builder.sitesBg;
+        this.assembly = builder.assembly;
+        this.logoCovariate = builder.logoCovariate;
+        this.createLogo = builder.createLogo;
+        this.tracks = builder.tracks;
+        this.batchSites = builder.batchSites;
+        this.task = builder.task;
     }
 
 
-    public BackendCommand(List<Sites> sites, Sites background, Task task) {
-        this.covariants = new ArrayList<>();
-        this.sites = null;
-        this.minBg = sites.get(0).getPositionCount() < 10000 ? 10000 : sites.get(0).getPositionCount();
-        this.customTracks = new ArrayList<>();
-        this.assembly = sites.get(0).getAssembly();
-        this.logoCovariate = false;
-        this.createLogo = false;
-        this.sitesBg = background;
-        this.tracks = Collections.emptyList();
-        this.task = task;
-        batchSites = sites;
-    }
+    /**
+     * Inner class for builder pattern
+     */
+    public static class Builder {
+        // Required params
+        private final Genome.Assembly assembly;
+        private final Task task;
 
 
-    public BackendCommand(Sites sites, Task task) {
-        this.covariants = new ArrayList<>();
-        this.sites = sites;
-        this.minBg = sites.getPositionCount() < 10000 ? 10000: sites.getPositionCount();
-        this.customTracks = new ArrayList<>();
-        this.assembly = sites.getAssembly();
-        this.logoCovariate = false;
-        this.createLogo = false;
-        this.sitesBg = null;
-        this.tracks = Collections.emptyList();
-        this.task = task;
-        batchSites = null;
+        // Optional params
+        private List<String> covariants = Collections.emptyList(); //list of ids of tracks that are used as covariant
+        private int minBg = 10000; //minimum of expected background positions
+        private List<Track> customTracks = Collections.emptyList();
+        private Sites sites = null;
+        private Sites sitesBg = null;
+        private boolean logoCovariate = false;
+        private boolean createLogo = false;
+        private List<String> tracks = Collections.emptyList();
+        private List<Sites> batchSites = Collections.emptyList();
+
+
+        public Builder(Task task, Genome.Assembly assembly) {
+
+            this.task = task;
+            this.assembly = assembly;
+        }
+
+        public BackendCommand build() {
+            check();
+
+            return new BackendCommand(this);
+        }
+
+        /**
+         * Makes a sanity check on the configuration.
+         *
+         * @throws RuntimeException - if anything is missing or not working in this combination
+         */
+        private void check() throws RuntimeException {
+
+            if (task == Task.ANALZYE_SINGLE) {
+                if (Objects.isNull(sites))
+                    throw new RuntimeException("No sites supplied for backend command");
+            }
+
+
+            if (task == Task.ANALYZE_BATCH) {
+                if (batchSites.size() == 0)
+                    throw new RuntimeException("No batch sites supplied for backend command");
+                if (Objects.nonNull(sites))
+                    throw new RuntimeException("Additional sites added in backend command for batch analysis");
+            }
+        }
+
+
+        public Builder covariants(List<String> val) {
+            this.covariants = val;
+            return this;
+        }
+
+        public Builder minBg(int val) {
+            this.minBg = val;
+            return this;
+        }
+
+        public Builder customTracks(List<Track> val) {
+            this.customTracks = val;
+            return this;
+        }
+
+        public Builder sites(Sites val) {
+            this.sites = val;
+            return this;
+        }
+
+        public Builder sitesBg(Sites val) {
+            this.sitesBg = val;
+            return this;
+        }
+
+
+        public Builder logoCovariate(boolean val) {
+            this.logoCovariate = val;
+            return this;
+        }
+
+
+        public Builder createLogo(boolean val) {
+            this.createLogo = val;
+            return this;
+        }
+
+        public Builder tracks(List<String> val) {
+            this.tracks = val;
+            return this;
+        }
+
+        public Builder batchSites(List<Sites> val) {
+            this.batchSites = val;
+            return this;
+        }
     }
 
-    public BackendCommand(Sites sites, Sites sitesBg, Task task) {
-        this.sitesBg = sitesBg;
-        this.sites = sites;
-        this.covariants = new ArrayList<>();
-        this.minBg = sites.getPositionCount();
-        this.customTracks = new ArrayList<>();
-        this.assembly = sites.getAssembly();
-        this.logoCovariate = false;
-        this.createLogo = false;
-        this.tracks = Collections.emptyList();
-        this.task = task;
-        batchSites = null;
-    }
-
-    public BackendCommand(InterfaceCommand command, Task task) {
-        this.covariants = command.getCovariants();
-        this.minBg = command.getMinBg();
-        this.sites = command.getSites();
-        this.customTracks = new ArrayList<>();
-        this.assembly = Genome.Assembly.valueOf(command.getAssembly());
-        this.logoCovariate = command.getLogoCovariate();
-        this.createLogo = command.getLogo() || this.logoCovariate;
-        this.sitesBg = command.getSitesBg();
-        this.tracks =  command.getTracks();
-        this.task = task;
-        batchSites = null;
-    }
 
     /**
      * Constructor to get all tracks from bg.
