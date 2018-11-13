cdaa4f26d3d6065f6199002b4ef38bc82b7f9704
Michael Menzel <michael.menzel@mni.thm.de>
2018-11-13 11:18:08 +0100

    Fixed tests

--- backend/src/test/java/de/thm/stat/EffectSizeTest.java
+++ backend/src/test/java/de/thm/stat/EffectSizeTest.java
@@ -29,9 +29,9 @@ public class EffectSizeTest {
     public void testFoldChange() throws Exception {
         EffectSize fc = new EffectSize();
 
-        assertEquals(0.122216, fc.foldChange(6000, 4000, 4700, 5300), 0.0001);
-        assertEquals(0.514548, fc.foldChange(1139, 5, 9021, 131), 0.1);
-        assertEquals(1.07, fc.foldChange(0, 1144, 86, 9066), 0.1);
+        assertEquals(0.405, fc.foldChange(6000, 4000, 4700, 5300), 0.01);
+        assertEquals(1.533, fc.foldChange(1139, 5, 9021, 131), 0.1);
+        assertEquals(3.55, fc.foldChange(0, 1144, 86, 9066), 0.1);
         assertEquals(0.0, fc.foldChange(0, 0, 0, 0), 0.0001);
         assertEquals(0.0, fc.foldChange(10, 10, 10, 10), 0.0001);
         assertEquals(0.0, fc.foldChange(25395, 0, 10000, 0), 0.0001);
